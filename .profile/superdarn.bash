3020bac5af50ddaf2ae1799afacd673165c26271
ajribeiro <ajribeiro86@gmail.com>
2012-10-19 11:14:00 -0400

    changed hdw.dat paths

--- .profile/superdarn.bash
+++ .profile/superdarn.bash
@@ -4,8 +4,8 @@
 
 export ISTP_PATH="/data/istp"
 
-export SD_HDWPATH="${RSTPATH}/tables/superdarn/hdw/"
-export SD_RADAR="${RSTPATH}/tables/superdarn/radar.dat"
+export SD_HDWPATH="/davit/lib/tables/superdarn/hdw/"
+export SD_RADAR="/davit/lib/tables/superdarn/radar.dat"
 
 export AACGM_DAT_PREFIX=${RSTPATH}"/tables/analysis/aacgm/aacgm_coeffs"
 export IGRF_PATH=${RSTPATH}"/tables/analysis/mag/"
