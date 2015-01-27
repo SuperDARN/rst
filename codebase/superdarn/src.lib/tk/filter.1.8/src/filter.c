4acef3a7706c9836ef3b3f612349f95e8cc3b7ec
davit.ece.vt.edu <davit@davit.ece.vt.edu>
2015-01-27 20:58:10 +0000

    Cleaning up testing

--- codebase/superdarn/src.lib/tk/filter.1.8/src/filter.c
+++ codebase/superdarn/src.lib/tk/filter.1.8/src/filter.c
@@ -139,6 +139,7 @@ int FilterRadarScan(int mode,int depth,int inx,struct RadarScan **src,
       if (bmcnt[bm][z]>mxbm) mxbm=bmcnt[bm][z];
     }
     if (n !=src[i]->num) break;
+    if (z == depth) exit(-1);
   }
 
   if (z !=depth) {
