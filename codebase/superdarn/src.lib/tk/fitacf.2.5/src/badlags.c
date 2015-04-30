51d365ca8946256e61be29a99e6b58b8e924fffd
davit.ece.vt.edu <davit@davit.ece.vt.edu>
2015-04-30 14:27:10 +0000

    Removing spaces

--- codebase/superdarn/src.lib/tk/fitacf.2.5/src/badlags.c
+++ codebase/superdarn/src.lib/tk/fitacf.2.5/src/badlags.c
@@ -5,26 +5,26 @@
 
 /*
  LICENSE AND DISCLAIMER
- 
+
  Copyright (c) 2012 The Johns Hopkins University/Applied Physics Laboratory
- 
+
  This file is part of the Radar Software Toolkit (RST).
- 
+
  RST is free software: you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  any later version.
- 
+
  RST is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU Lesser General Public License for more details.
- 
+
  You should have received a copy of the GNU Lesser General Public License
  along with RST.  If not, see <http://www.gnu.org/licenses/>.
- 
- 
- 
+
+
+
 */
 
 #include <stdio.h>
@@ -68,7 +68,7 @@ void FitACFBadlags(struct FitPrm *ptr,struct FitACFBadSample *bptr) {
 	  t1 = (long) (ptr->pulse[i]) * (long) (ptr->mpinc)
 				- ptr->txpl/2;
 	  t2 = t1 + 3*ptr->txpl/2 + 100; /* adjust for rx-on delay */
-	}	
+	}
 
 	/*	we now have a pulse that occurs after the current sample.  Start
 		incrementing the sample number until we find a sample that lies
@@ -78,7 +78,7 @@ void FitACFBadlags(struct FitPrm *ptr,struct FitACFBadSample *bptr) {
       sample++;
 	  ts = ts + ptr->smsep;
 	}
-	
+
 	/*	ok, we now have a sample which occurs after the pulse starts.
 		check to see if it occurs before the pulse ends, and if so, mark
 		it as a bad sample */
@@ -111,18 +111,18 @@ void FitACFCkRng(int range,int *badlag,struct FitACFBadSample *bptr,
 			+ range - 1;
 
 	for (j=0; j<bptr->nbad; j++) {
-      if ((sam1 == bptr->badsmp[j]) || (sam2 == bptr->badsmp[j])) 
+      if ((sam1 == bptr->badsmp[j]) || (sam2 == bptr->badsmp[j]))
         badlag[i] = 1;
 	  if (sam2 < bptr->badsmp[j]) break;
     }
   }
-	
+
   /* This section of code is only of use to fitacf for reprocessing old
 	data that used the 16 lag, 7 pulse code.  */
-	
+
   if ((ptr->mplgs == 17) && (ptr->old !=0) )
 	badlag[13] = 1;
-	 
+
   /* finally, check for range interference */
   lag_overlap(range, badlag, ptr);
   return;
