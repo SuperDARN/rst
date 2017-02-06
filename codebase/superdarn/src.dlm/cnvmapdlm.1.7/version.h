a22296bd69883a7e264cf23449e6a1907c1134f7
egthomas <evan.g.thomas@dartmouth.edu>
2017-02-06 13:57:05 -0500

    Cleaning up and ignoring more files

--- codebase/superdarn/src.dlm/cnvmapdlm.1.7/version.h
+++
@@ -1,5 +0,0 @@
-/*version.h
-  =========*/
-
- #define MAJOR_VERSION "x"
- #define MINOR_VERSION "x"
--- codebase/superdarn/src.lib/tk/fitacfT.2.4/src/version.h
+++
@@ -1,5 +0,0 @@
-/*version.h
-  =========*/
-
- #define MAJOR_VERSION "2"
- #define MINOR_VERSION "4"
