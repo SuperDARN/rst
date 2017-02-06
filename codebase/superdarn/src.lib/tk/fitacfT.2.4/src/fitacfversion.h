90ad1038bcbc515c5c1951c3abfd09a9cafd804b
egthomas <evan.g.thomas@dartmouth.edu>
2017-02-06 14:48:22 -0500

    Adding fitacfversion.h files back

---
+++ codebase/superdarn/src.lib/tk/fitacfT.2.4/src/fitacfversion.h
@@ -0,0 +1,17 @@
+/* fitacfversion.h
+   ===============
+   Author: R.J.Barnes & K.Baker
+*/
+
+/*
+ (c) 2010 JHU/APL & Others - Please Consult LICENSE.superdarn-rst.3.2-beta-4-g32f7302.txt for more information.
+ 
+ 
+ 
+*/
+
+
+
+
+#define FITACF_MAJOR_REVISION 5
+#define FITACF_MINOR_REVISION 0
