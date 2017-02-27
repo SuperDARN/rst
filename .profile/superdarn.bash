3ca0bafefc1b28dca0731811d4f50ebe24f94f57
egthomas <evan.g.thomas@dartmouth.edu>
2017-02-27 13:26:54 -0500

    Adding AACGM_v2 and IGRF12 environment variables to superdarn bash and tcsh profiles

--- .profile/superdarn.bash
+++ .profile/superdarn.bash
@@ -1,7 +1,6 @@
 # superdarn.bash
 # ==============
 
-
 export ISTP_PATH="/data/istp"
 
 export SD_HDWPATH="${RSTPATH}/tables/superdarn/hdw/"
@@ -11,7 +10,6 @@ export AACGM_DAT_PREFIX=${RSTPATH}"/tables/analysis/aacgm/aacgm_coeffs"
 export IGRF_PATH=${RSTPATH}"/tables/analysis/mag/"
 export SD_MODEL_TABLE=${RSTPATH}"/tables/superdarn/model"
 
-
-
-
+export AACGM_v2_DAT_PREFIX="${RSTPATH}/tables/analysis/aacgm/aacgm_coeffs-12-"
+export IGRF_COEFFS="${IGRF_PATH}igrf12coeffs.txt"
 
