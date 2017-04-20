be8d034606cc9f9a46f221f8dcb86354e6569954
egthomas <evan.g.thomas@dartmouth.edu>
2017-04-20 08:16:32 -0400

    Removing empty overlay.dat file from tables and unused MAPOVERLY environment variable

--- .profile/general.bash
+++ .profile/general.bash
@@ -3,12 +3,4 @@
 
 export MAPDATA=${RSTPATH}"/tables/general/map_data"
 export BNDDATA=${RSTPATH}"/tables/general/bnd_data"
-export MAPOVERLAY=${RSTPATH}"/tables/general/overlay.dat"
-
-
-
-
-
-
-
 
