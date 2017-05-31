2d1943d7d9e972d116f515c55d6afed58012810e
egthomas <evan.g.thomas@dartmouth.edu>
2017-05-31 16:07:17 -0400

    Implementing DLM functionality for AACGM_v2 library
    This commit introduces DLM functionality for the AACGM_v2_SetNow(),
    AACGM_v2_SetDateTime(), and AACGM_v2_Convert() functions in IDL.  Note
    that the AACGM_v2_GetDateTime() function does not yet work (need to
    figure out how to return values via keywords).

--- codebase/analysis/src.dlm/aacgmdlm.2.0/aacgmdlm.c
+++ codebase/analysis/src.dlm/aacgmdlm.2.0/aacgmdlm.c
@@ -19,6 +19,7 @@
 #include "idl_export.h"
 #include "rtypes.h"
 #include "aacgm.h"
+#include "aacgmlib_v2.h"
 
 #define AACGM_ERROR 0
 #define AACGM_MISMATCHDIMS -1
@@ -227,13 +228,253 @@ static IDL_VPTR IDLAACGMConvert(int argc,IDL_VPTR *argv,char *argk) {
 
 
 
+static IDL_VPTR IDLAACGM_v2_GetDateTime(int argc,IDL_VPTR *argv,char *argk) {
+
+    int s=0;
+    int yr,mo,dy,hr,mt,sc,dayno;
+    int outargc;
+    IDL_VPTR outargv[0];
+
+    static IDL_LONG month;
+    static IDL_LONG day;
+    static IDL_LONG hour;
+    static IDL_LONG minute;
+    static IDL_LONG second;
+    static IDL_LONG dyno;
+    
+    static IDL_KW_PAR kw_pars[]={IDL_KW_FAST_SCAN,
+        {"DAY",IDL_TYP_LONG,1,IDL_KW_ZERO,0,IDL_CHARA(day)},
+        {"DYNO",IDL_TYP_LONG,1,IDL_KW_ZERO,0,IDL_CHARA(dyno)},
+        {"HOUR",IDL_TYP_LONG,1,IDL_KW_ZERO,0,IDL_CHARA(hour)},
+        {"MINUTE",IDL_TYP_LONG,1,IDL_KW_ZERO,0,IDL_CHARA(minute)},
+        {"MONTH",IDL_TYP_LONG,1,IDL_KW_ZERO,0,IDL_CHARA(month)},
+        {"SECOND",IDL_TYP_LONG,1,IDL_KW_ZERO,0,IDL_CHARA(second)},
+        {NULL}};
+
+    IDL_KWCleanup(IDL_KW_MARK);
+    outargc=IDL_KWGetParams(argc,argv,argk,kw_pars,outargv,1);
+
+    s=AACGM_v2_GetDateTime(&yr,&mo,&dy,&hr,&mt,&sc,&dayno);
+
+    IDL_StoreScalar(outargv[0],IDL_TYP_DOUBLE,(IDL_ALLTYPES *) &yr);
+    if (month) IDL_StoreScalar(month,IDL_TYP_DOUBLE,(IDL_ALLTYPES *) &mo);
+    if (day) IDL_StoreScalar(day,IDL_TYP_DOUBLE,(IDL_ALLTYPES *) &dy);
+    if (hour) IDL_StoreScalar(hour,IDL_TYP_DOUBLE,(IDL_ALLTYPES *) &hr);
+    if (minute) IDL_StoreScalar(minute,IDL_TYP_DOUBLE,(IDL_ALLTYPES *) &mt);
+    if (second) IDL_StoreScalar(second,IDL_TYP_DOUBLE,(IDL_ALLTYPES *) &sc);
+    if (dyno) IDL_StoreScalar(dyno,IDL_TYP_DOUBLE,(IDL_ALLTYPES *) &dayno);
+
+    IDL_KWCleanup(IDL_KW_CLEAN);
+    return (IDL_GettmpLong(s));
+}
+
+
+
+static IDL_VPTR IDLAACGM_v2_SetDateTime(int argc,IDL_VPTR *argv) {
+
+    int s=0;
+    int yr,mo,dy,hr,mt,sc;
+
+    IDL_ENSURE_SCALAR(argv[0]);
+    yr=IDL_LongScalar(argv[0]);
+    mo=1;
+    dy=1;
+    hr=0;
+    mt=0;
+    sc=0;
+
+    if (argc > 5) {
+        IDL_ENSURE_SCALAR(argv[5]);
+        sc=IDL_LongScalar(argv[5]);
+    }
+
+    if (argc > 4) {
+        IDL_ENSURE_SCALAR(argv[4]);
+        mt=IDL_LongScalar(argv[4]);
+    }
+
+    if (argc > 3) {
+        IDL_ENSURE_SCALAR(argv[3]);
+        hr=IDL_LongScalar(argv[3]);
+    }
+
+    if (argc > 2) {
+        IDL_ENSURE_SCALAR(argv[2]);
+        dy=IDL_LongScalar(argv[2]);
+    }
+
+    if (argc > 1) {
+        IDL_ENSURE_SCALAR(argv[1]);
+        mo=IDL_LongScalar(argv[1]);
+    }
+
+    s=AACGM_v2_SetDateTime(yr,mo,dy,hr,mt,sc);
+
+    return (IDL_GettmpLong(s));
+}
+
+
+
+static IDL_VPTR IDLAACGM_v2_SetNow(int argc,IDL_VPTR *argv) {
+
+    int s=0;
+    s=AACGM_v2_SetNow();
+
+    return (IDL_GettmpLong(s));
+}
+
+
+
+static IDL_VPTR IDLAACGM_v2_Convert(int argc,IDL_VPTR *argv,char *argk) {
+
+    int s=0,n;
+    int outargc;
+    IDL_VPTR outargv[6];
+    IDL_VPTR volat,volon,vr;
+
+    double inlat,inlon,height;
+    double outlat,outlon,r;
+    double *latptr,*lonptr,*rptr;
+
+    int code=0;
+
+    static IDL_LONG allowtrace;
+    static IDL_LONG badidea;
+    static IDL_LONG gcentric;
+    static IDL_LONG geo;
+    static IDL_LONG trace;
+
+    static IDL_KW_PAR kw_pars[]={IDL_KW_FAST_SCAN,
+        {"ALLOW_TRACE",IDL_TYP_LONG,1,IDL_KW_ZERO,0,IDL_CHARA(allowtrace)},
+        {"BAD_IDEA",IDL_TYP_LONG,1,IDL_KW_ZERO,0,IDL_CHARA(badidea)},
+        {"GCENTRIC",IDL_TYP_LONG,1,IDL_KW_ZERO,0,IDL_CHARA(gcentric)},
+        {"GEO",IDL_TYP_LONG,1,IDL_KW_ZERO,0,IDL_CHARA(geo)},
+        {"TRACE",IDL_TYP_LONG,1,IDL_KW_ZERO,0,IDL_CHARA(trace)},
+        {NULL}};
+
+    IDL_KWCleanup(IDL_KW_MARK);
+    outargc=IDL_KWGetParams(argc,argv,argk,kw_pars,outargv,1);
+
+    IDL_EXCLUDE_EXPR(outargv[3]);
+    IDL_EXCLUDE_EXPR(outargv[4]);
+    IDL_EXCLUDE_EXPR(outargv[5]);
+
+    if (geo) {
+        code=1;
+    }
+
+    if (trace) {
+        code=2;
+    }
+
+    if (gcentric) {
+        code=16;
+    }
+
+    if (argv[1]->flags & IDL_V_ARR) {
+        int nval;
+        IDL_ENSURE_ARRAY(outargv[1]);
+        IDL_ENSURE_ARRAY(outargv[2]);
+
+        nval=outargv[0]->value.arr->n_elts;
+        if ( (outargv[1]->value.arr->n_elts !=nval) ||
+             (outargv[2]->value.arr->n_elts !=nval)) {
+
+            /* array size mismatch */
+            IDL_MessageFromBlock(msg_block,AACGM_MISMATCHELMS,IDL_MSG_LONGJMP,
+                                    "in AACGM_v2_Convert()");
+        }
+
+        if ((outargv[0]->type !=IDL_TYP_FLOAT) && 
+            (outargv[0]->type !=IDL_TYP_DOUBLE)) {
+
+            /* bad type */
+            IDL_MessageFromBlock(msg_block,AACGM_BADTYPE,IDL_MSG_LONGJMP,
+                                    "of lat in AACGM_v2_Convert()");
+        }
+
+        if ((outargv[1]->type !=IDL_TYP_FLOAT) && 
+            (outargv[1]->type !=IDL_TYP_DOUBLE)) {
+
+            /* bad type */
+            IDL_MessageFromBlock(msg_block,AACGM_BADTYPE,IDL_MSG_LONGJMP,
+                                    "of lon in AACGM_v2_Convert()");
+        }
+
+        if ((outargv[2]->type !=IDL_TYP_FLOAT) && 
+            (outargv[2]->type !=IDL_TYP_DOUBLE)) {
+
+            /* bad type */
+            IDL_MessageFromBlock(msg_block,AACGM_BADTYPE,IDL_MSG_LONGJMP,
+                                    "of height in AACGM_v2_Convert()");
+        }
+
+        latptr=(double *) IDL_MakeTempArray(IDL_TYP_DOUBLE,
+                                            outargv[0]->value.arr->n_dim,
+                                            outargv[0]->value.arr->dim,
+                                            IDL_ARR_INI_ZERO,&volat);
+
+        lonptr=(double *) IDL_MakeTempArray(IDL_TYP_DOUBLE,
+                                            outargv[0]->value.arr->n_dim,
+                                            outargv[0]->value.arr->dim,
+                                            IDL_ARR_INI_ZERO,&volon);
+
+        rptr=(double *) IDL_MakeTempArray(IDL_TYP_DOUBLE,
+                                          outargv[0]->value.arr->n_dim,
+                                          outargv[0]->value.arr->dim,
+                                          IDL_ARR_INI_ZERO,&vr);
+
+        for (n=0;n<nval;n++) {
+            if (outargv[0]->type==IDL_TYP_FLOAT) inlat=
+                ((float *) outargv[0]->value.arr->data)[n];
+            else inlat=((double *) outargv[0]->value.arr->data)[n];
+            if (outargv[1]->type==IDL_TYP_FLOAT) inlon=
+                ((float *) outargv[1]->value.arr->data)[n];
+            else inlon=((double *) outargv[1]->value.arr->data)[n];
+            if (outargv[2]->type==IDL_TYP_FLOAT) height=
+                ((float *) outargv[2]->value.arr->data)[n];
+            else height=((double *) outargv[2]->value.arr->data)[n];
+
+            s=AACGM_v2_Convert(inlat,inlon,height,&latptr[n],&lonptr[n],
+                            &rptr[n],code);
+        }
+
+        IDL_VarCopy(volat,argv[3]);
+        IDL_VarCopy(volon,argv[4]);
+        IDL_VarCopy(vr,argv[5]);
+
+    } else {
+        /* scalar */
+
+        IDL_ENSURE_SCALAR(outargv[1]);
+        IDL_ENSURE_SCALAR(outargv[2]);
+
+        inlat=IDL_DoubleScalar(outargv[0]);
+        inlon=IDL_DoubleScalar(outargv[1]);
+        height=IDL_DoubleScalar(outargv[2]);
+
+        s=AACGM_v2_Convert(inlat,inlon,height,&outlat,&outlon,&r,code);
+        IDL_StoreScalar(outargv[3],IDL_TYP_DOUBLE,(IDL_ALLTYPES *) &outlat);
+        IDL_StoreScalar(outargv[4],IDL_TYP_DOUBLE,(IDL_ALLTYPES *) &outlon);
+        IDL_StoreScalar(outargv[5],IDL_TYP_DOUBLE,(IDL_ALLTYPES *) &r);
+    }
+
+    IDL_KWCleanup(IDL_KW_CLEAN);
+    return (IDL_GettmpLong(code));
+}
+
+
+
 int IDL_Load(void) {
 
   static IDL_SYSFUN_DEF2 fnaddr[]={
     { IDLAACGMInit,"AACGMINIT",1,1,0,0},
     { IDLAACGMLoadCoef,"AACGMLOADCOEF",1,1,0,0},
     { IDLAACGMConvert,"AACGMCONVERT",6,6,IDL_SYSFUN_DEF_F_KEYWORDS,0},
-   
+    { IDLAACGM_v2_GetDateTime,"AACGM_V2_GETDATETIME",1,1,IDL_SYSFUN_DEF_F_KEYWORDS,0},   
+    { IDLAACGM_v2_SetDateTime,"AACGM_V2_SETDATETIME",1,6,0,0},
+    { IDLAACGM_v2_SetNow,"AACGM_V2_SETNOW",0,0,0,0},
+    { IDLAACGM_v2_Convert,"AACGM_V2_CONVERT",6,6,IDL_SYSFUN_DEF_F_KEYWORDS,0},   
   };
 
 
