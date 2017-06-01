c3a1f0b272c7045bb4a950be5bfc2d141fd9aa2b
sshepherd <simon.shepherd@dartmouth.edu>
2017-06-01 14:41:04 -0400

    Adding logic to allow MLT-v2 functions (C and IDL) to set the AACGM-v2 date/time with arguments passed into the MLT-v2 functions. If the AACGM-v2 date/time are not set OR differ by 30 days from the MLT-v2 values, the AACGM-v2 date/time is now set.

--- codebase/analysis/src.lib/mlt_v2/mlt.1.0/src/mlt_v2.c
+++ codebase/analysis/src.lib/mlt_v2/mlt.1.0/src/mlt_v2.c
@@ -6,15 +6,65 @@
 #include "rtime.h"
 #include "astalg.h"
 
+/*-----------------------------------------------------------------------------
+  MLT functions for use with AACGM-v2
+;
+; C user functions intended to compute the magnetic local time at a given
+; time and location given by AACGM-v2 coordinates.
+;
+; Code based on legacy routines authored by Kile Baker, Simon Wing and Robin
+; Barnes.
+;
+; Significant changes include:
+;
+; - discontinued use of common block used for interpolation between two values
+;   separated by 10 minutes.
+;
+; - MLT is based on the relative AACGM-v2 longitudes of the desired location to
+;   that of a reference location. The reference location is taken to be the
+;   subsolar point at 700 km altitude. The reference AACGM-v2 longitude is
+;   computed using AACGM-v2 coefficients, which act to interpolate through
+;   regions where AACGM coordinates are undefined, thereby providing a
+;   mechanism for defining the reference longitude in a consistent manner.
+;   Differences with values determined at 0 km altitude (where defined) are
+;   typically <1 minute and always <5 minutes (in MLT).
 
-/* Changes
+; 20170601 SGS v1.2  MLTConvert_v2 now calls AACGM_v2_SetDateTime() if the
+;                    AACGM-v2 date/time is not currently set OR if the
+;                    date/time passed into one of the public functions differs
+;                    from the AACGM-v2 date/time by more than 30 days. In each
+;                    case the AACGM-v2 coefficients are loaded and interpolated
+;                    which could impact other calls to AACGM_v2_Convert() if
+;                    the date/time is not reset.
+
+; Changes
 
    Combined MLTAst and MLTAst1
 
    Removed *mlson variable since it is only used for doing linear
      interpolation between two periods separated by 10 minutes.
-*/
 
+;
+; Public Functions:
+; -----------------
+;
+; mlt  = MLTConvertYMDHMS_v2(yr,mo,dy,hr,mt,sc, mlon);
+; mlon = inv_MLTConvertYMDHMS_v2(yr,mo,dy,hr,mt,sc, mlt);
+;
+; mlt  = MLTConvertEpoch_v2(epoch, mlon);
+; mlon = inv_MLTConvertEpoch_v2(epoch, mlt);
+;
+; mlt  = MLTConvertYrsec_v2(yr,yrsec, mlon);
+; mlon = inv_MLTConvertYrsec_v2(yr,yrsec, mlt);
+;
+; Private Functions:
+; ------------------
+;
+; mlt  = MLTConvert_v2(yr,mo,dy,hr,mt,sc, mlon);
+; mlon = inv_MLTConvert_v2(yr,mo,dy,hr,mt,sc, mlt);
+; 
+;
+*/
 
 double mlon_ref = -1;
 
@@ -38,9 +88,27 @@ double MLTConvert_v2(int yr, int mo, int dy, int hr, int mt ,int sc,
                       double mlon)
 {
   int err;
+  int ayr,amo,ady,ahr,amt,asc,adyn;
   double dd,jd,eqt,dec,ut,at;
   double slon,mlat,r;
   double hgt,aacgm_mlt;
+  double ajd;
+
+  AACGM_v2_GetDateTime(&ayr, &amo, &ady, &ahr, &amt, &asc, &adyn);
+  if (ayr < 0) { 
+    /* AACGM date/time not set so set it to the date/time passed in */
+    err = AACGM_v2_SetDateTime(yr,mo,dy,hr,mt,sc);
+    if (err != 0) return (err);
+  } else {
+    /* If date/time passed into function differs from AACGM data/time by more
+     * than 30 days, recompute the AACGM-v2 coefficients */
+    ajd = TimeYMDHMSToJulian(ayr,amo,ady,ahr,amt,asc);
+    jd =  TimeYMDHMSToJulian(yr,mo,dy,hr,mt,sc);
+    if (abs(jd-ajd) > 30) {
+      err = AACGM_v2_SetDateTime(yr,mo,dy,hr,mt,sc);
+    }
+    if (err != 0) return (err);
+  }
 
 /* check for bad input, which can come from undefined region, and return NAN */
   if (!isfinite(mlon)) {
