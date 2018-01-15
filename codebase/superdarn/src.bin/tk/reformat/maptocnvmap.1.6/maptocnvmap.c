d93e7c6d1e2426c8f44a83aaaae2fccfb13f4154
egthomas <evan.g.thomas@dartmouth.edu>
2018-01-15 11:03:50 -0500

    Fixing error check in gridtogrdmap and maptocnvmap
    This commit fixes a bug in the way gridtogrdmap and maptocnvmap check
    for errors which was added in the warning_cleanup branch prior to the
    4.1 software release. The error check added to the end of these binaries
    looks for a non-zero return value from GridFwrite and CnvmapFwrite
    respectively, when it should actually check for values less than or
    equal to zero. The check for non-zero return values was only appropriate
    for the OldGridFwrite and OldCnvMapFwrite functions in grdmaptogrid and
    cnvmaptomap respectively. This commit makes the necessary changes so
    that gridtogrdmap and maptocnvmap do not always return a
    "GridFwrite/CnvmapFwrite failed." error message.

--- codebase/superdarn/src.bin/tk/reformat/maptocnvmap.1.6/maptocnvmap.c
+++ codebase/superdarn/src.bin/tk/reformat/maptocnvmap.1.6/maptocnvmap.c
@@ -104,7 +104,7 @@ int main (int argc,char *argv[]) {
               yr,mo,dy,hr,mt,(int) sc);
     }
     s=CnvMapFwrite(stdout,map,grd);
-    if (s !=0) {
+    if (s <=0) {
         fprintf(stderr,"CnvMapFwrite failed.\n");
         if (fp !=stdin) fclose(fp);
         exit(-1);
