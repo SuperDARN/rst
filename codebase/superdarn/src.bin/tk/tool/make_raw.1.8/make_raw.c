7d62b93cb9c893276131bacda73e0839f168a23f
egthomas <evan.g.thomas@dartmouth.edu>
2018-04-06 12:24:32 -0400

    Implementing option error handling for make_raw

--- codebase/superdarn/src.bin/tk/tool/make_raw.1.8/make_raw.c
+++ codebase/superdarn/src.bin/tk/tool/make_raw.1.8/make_raw.c
@@ -88,6 +88,11 @@ int loadlag(FILE *fp,int *lag[2]) {
   return lagnum-1;
 }
 
+int rst_opterr(char *txt) {
+  fprintf(stderr,"Option not recognized: %s\n",txt);
+  fprintf(stderr,"Please try: make_raw --help\n");
+  return(-1);
+}
 
 int main (int argc,char *argv[]) {
 
@@ -168,7 +173,11 @@ int main (int argc,char *argv[]) {
   OptionAdd(&opt,"qi",'x',&qiflg);
   OptionAdd(&opt,"skip",'i',&skpval);
 
-  arg=OptionProcess(1,argc,argv,&opt,NULL);
+  arg=OptionProcess(1,argc,argv,&opt,rst_opterr);
+
+  if (arg==-1) {
+    exit(-1);
+  }
 
   if (help==1) {
     OptionPrintInfo(stdout,hlpstr);
