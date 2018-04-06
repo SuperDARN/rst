227d3d79006d6f4d2e0e203efb6973488ed2fa80
egthomas <evan.g.thomas@dartmouth.edu>
2018-04-06 12:07:21 -0400

    Implementing option error handling for rtfitacftofit

--- codebase/superdarn/src.bin/tk/tcpip/rtfitacftofit.1.19/rtfitacftofit.c
+++ codebase/superdarn/src.bin/tk/tcpip/rtfitacftofit.1.19/rtfitacftofit.c
@@ -83,6 +83,12 @@ void trappipe(int signal) {
   outpipe=-1;
 }
 
+int rst_opterr(char *txt) {
+  fprintf(stderr,"Option not recognized: %s\n",txt);
+  fprintf(stderr,"Please try: rtfitacftofit --help\n");
+  return(-1);
+}
+
 int main(int argc,char *argv[]) {
   int arg;
   unsigned char help=0;
@@ -139,7 +145,11 @@ int main(int argc,char *argv[]) {
   OptionAdd(&opt,"pf",'t',&pnamestr);
   OptionAdd(&opt,"if",'t',&pidstr);
  
-  arg=OptionProcess(1,argc,argv,&opt,NULL);   
+  arg=OptionProcess(1,argc,argv,&opt,rst_opterr);
+
+  if (arg==-1) {
+    exit(-1);
+  }
 
   if (help==1) {
     OptionPrintInfo(stdout,hlpstr);
