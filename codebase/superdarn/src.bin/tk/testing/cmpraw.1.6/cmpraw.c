faee6f52dc281e7015fad7232cf20508d7fc9de7
egthomas <evan.g.thomas@dartmouth.edu>
2018-04-06 11:58:28 -0400

    Implementing option error handling for cmpraw

--- codebase/superdarn/src.bin/tk/testing/cmpraw.1.6/cmpraw.c
+++ codebase/superdarn/src.bin/tk/testing/cmpraw.1.6/cmpraw.c
@@ -53,6 +53,12 @@ struct rawdata rawB;
 
 struct OptionData opt;
 
+int rst_opterr(char *txt) {
+  fprintf(stderr,"Option not recognized: %s\n",txt);
+  fprintf(stderr,"Please try: cmpraw --help\n");
+  return(-1);
+}
+
 int main(int argc,char *argv[]) {
 
   unsigned char help=0;
@@ -70,7 +76,11 @@ int main(int argc,char *argv[]) {
   OptionAdd(&opt,"-help",'x',&help);
   OptionAdd(&opt,"-option",'x',&option);
  
-  arg=OptionProcess(1,argc,argv,&opt,NULL);  
+  arg=OptionProcess(1,argc,argv,&opt,rst_opterr);
+
+  if (arg==-1) {
+    exit(-1);
+  }
 
   if (help==1) {
     OptionPrintInfo(stdout,hlpstr);
