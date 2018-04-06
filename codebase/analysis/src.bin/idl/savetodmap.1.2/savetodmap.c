a7e21a74aa4513322fa38e287ea9f1d94165e375
egthomas <evan.g.thomas@dartmouth.edu>
2018-04-06 10:24:19 -0400

    Implementing option error handling for savetodmap

--- codebase/analysis/src.bin/idl/savetodmap.1.2/savetodmap.c
+++ codebase/analysis/src.bin/idl/savetodmap.1.2/savetodmap.c
@@ -44,6 +44,12 @@
 struct OptionData opt;
 int arg=0;
 
+int rst_opterr(char *txt) {
+  fprintf(stderr,"Option not recognized: %s\n",txt);
+  fprintf(stderr,"Please try: savetodmap --help\n");
+  return(-1);
+}
+
 int main(int argc,char *argv[]) {
 
   unsigned char zflg=0;
@@ -57,7 +63,12 @@ int main(int argc,char *argv[]) {
   OptionAdd(&opt,"-option",'x',&option);
   OptionAdd(&opt,"z",'x',&zflg);
 
-  arg=OptionProcess(1,argc,argv,&opt,NULL);
+  arg=OptionProcess(1,argc,argv,&opt,rst_opterr);
+
+  if (arg==-1) {
+    exit(-1);
+  }
+
   if (help==1) {
     OptionPrintInfo(stdout,hlpstr);
     exit(0);
