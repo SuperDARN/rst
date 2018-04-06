7dcec97271150de96fd4e88e87c48091ee7c6147
egthomas <evan.g.thomas@dartmouth.edu>
2018-04-06 12:01:06 -0400

    Implementing option error handling for fitacfclient

--- codebase/superdarn/src.bin/tk/tcpip/fitacfclient.1.12/fitacfclient.c
+++ codebase/superdarn/src.bin/tk/tcpip/fitacfclient.1.12/fitacfclient.c
@@ -54,6 +54,12 @@
 
 struct OptionData opt;
 
+int rst_opterr(char *txt) {
+  fprintf(stderr,"Option not recognized: %s\n",txt);
+  fprintf(stderr,"Please try: fitacfclient --help\n");
+  return(-1);
+}
+
 int main(int argc,char *argv[]) {
   int arg;
   unsigned char help=0;
@@ -73,7 +79,11 @@ int main(int argc,char *argv[]) {
   OptionAdd(&opt,"-help",'x',&help);
   OptionAdd(&opt,"-option",'x',&option);
 
-  arg=OptionProcess(1,argc,argv,&opt,NULL);   
+  arg=OptionProcess(1,argc,argv,&opt,rst_opterr);
+
+  if (arg==-1) {
+    exit(-1);
+  }
 
   if (help==1) {
     OptionPrintInfo(stdout,hlpstr);
