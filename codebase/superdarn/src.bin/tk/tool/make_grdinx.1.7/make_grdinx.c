8a2cdb5e308f52c09c5d37def8a7e81674e23d10
egthomas <evan.g.thomas@dartmouth.edu>
2018-04-06 12:20:56 -0400

    Implementing option error handling for make_grdinx

--- codebase/superdarn/src.bin/tk/tool/make_grdinx.1.7/make_grdinx.c
+++ codebase/superdarn/src.bin/tk/tool/make_grdinx.1.7/make_grdinx.c
@@ -53,6 +53,11 @@
 struct OptionData opt;
 struct GridData *grd;
 
+int rst_opterr(char *txt) {
+  fprintf(stderr,"Option not recognized: %s\n",txt);
+  fprintf(stderr,"Please try: make_grdinx --help\n");
+  return(-1);
+}
 
 int main (int argc,char *argv[]) {
 
@@ -77,7 +82,11 @@ int main (int argc,char *argv[]) {
   OptionAdd(&opt,"old",'x',&old);
   OptionAdd(&opt,"vb",'x',&vb);
 
-  arg=OptionProcess(1,argc,argv,&opt,NULL);
+  arg=OptionProcess(1,argc,argv,&opt,rst_opterr);
+
+  if (arg==-1) {
+    exit(-1);
+  }
 
   if (help==1) {
     OptionPrintInfo(stdout,hlpstr);
