72985970d73b64061f6b7c33ed3323884f56d78d
egthomas <evan.g.thomas@dartmouth.edu>
2018-06-17 21:46:48 -0400

    Implementing option error handling for ppmxtopng

--- codebase/base/src.bin/graphic/ppmxtopng.1.2/ppmxtopng.c
+++ codebase/base/src.bin/graphic/ppmxtopng.1.2/ppmxtopng.c
@@ -36,6 +36,12 @@
 
 struct OptionData opt;
 
+int rst_opterr(char *txt) {
+  fprintf(stderr,"Option not recognized: %s\n",txt);
+  fprintf(stderr,"Please try: ppmxtopng --help\n");
+  return(-1);
+}
+
 int main(int argc,char *argv[]) {
  
   int arg=0;
@@ -49,7 +55,11 @@ int main(int argc,char *argv[]) {
   OptionAdd(&opt,"-help",'x',&help);  
   OptionAdd(&opt,"-option",'x',&option);
 
-  arg=OptionProcess(1,argc,argv,&opt,NULL);   
+  arg=OptionProcess(1,argc,argv,&opt,rst_opterr);
+
+  if (arg==-1) {
+    exit(-1);
+  }
 
   if (help==1) {
     OptionPrintInfo(stdout,hlpstr);
