180a221aedc01c870ffc05ca33bdd5f2dc9732b1
egthomas <evan.g.thomas@dartmouth.edu>
2018-06-25 16:55:33 -0400

    Implementing option error handling for scalexml

--- codebase/base/src.bin/graphic/scalexml.1.7/scalexml.c
+++ codebase/base/src.bin/graphic/scalexml.1.7/scalexml.c
@@ -48,6 +48,11 @@ int stream(char *buf,int sze,void *data) {
   return 0;
 }
 
+int rst_opterr(char *txt) {
+  fprintf(stderr,"Option not recognized: %s\n",txt);
+  fprintf(stderr,"Please try: scalexml --help\n");
+  return(-1);
+}
 
 int main(int argc,char *argv[]) {
   
@@ -81,7 +86,11 @@ int main(int argc,char *argv[]) {
   OptionAdd(&opt,"sf",'f',&sf);
  
 
-  arg=OptionProcess(1,argc,argv,&opt,NULL);   
+  arg=OptionProcess(1,argc,argv,&opt,rst_opterr);
+
+  if (arg==-1) {
+    exit(-1);
+  }
 
   if (help==1) {
     OptionPrintInfo(stdout,hlpstr);
