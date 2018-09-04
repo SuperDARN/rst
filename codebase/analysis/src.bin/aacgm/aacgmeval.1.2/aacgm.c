e532088768bee58dbc784958bddbcc7b396e35fd
egthomas <evan.g.thomas@dartmouth.edu>
2018-09-04 15:37:10 -0400

    Fixing resource leak ini aacgm.c of aacgmeval by calling fclose after reading input text file

--- codebase/analysis/src.bin/aacgm/aacgmeval.1.2/aacgm.c
+++ codebase/analysis/src.bin/aacgm/aacgmeval.1.2/aacgm.c
@@ -194,6 +194,7 @@ int main(int argc,char *argv[]) {
       else AACGM_v2_Convert(ilat,ilon,alt,&olat,&olon,&r,flag);
       fprintf(stdout,fmt,olat,olon);    
     }
+    fclose(fp);
   }
   return 0;
 }
