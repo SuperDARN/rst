36e78fe909ed4bdfca4e2ad1a14e28b1e9d9fbd9
davit.ece.vt.edu <davit@davit.ece.vt.edu>
2018-11-08 16:01:45 +0000

    Removing extra spaces

--- codebase/superdarn/src.bin/tk/testing/test_raw.1.12/test_raw.c
+++ codebase/superdarn/src.bin/tk/testing/test_raw.1.12/test_raw.c
@@ -5,26 +5,26 @@
 
 /*
  LICENSE AND DISCLAIMER
- 
+
  Copyright (c) 2012 The Johns Hopkins University/Applied Physics Laboratory
- 
+
  This file is part of the Radar Software Toolkit (RST).
- 
+
  RST is free software: you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  any later version.
- 
+
  RST is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU Lesser General Public License for more details.
- 
+
  You should have received a copy of the GNU Lesser General Public License
  along with RST.  If not, see <http://www.gnu.org/licenses/>.
- 
- 
- 
+
+
+
 */
 
 #include <stdio.h>
@@ -74,12 +74,12 @@ int main (int argc,char *argv[]) {
 
   prm=RadarParmMake();
   raw=RawMake();
- 
+
   OptionAdd(&opt,"-help",'x',&help);
   OptionAdd(&opt,"-option",'x',&option);
   OptionAdd(&opt,"-version",'x',&version);
 
-  OptionAdd(&opt,"old",'x',&old); 
+  OptionAdd(&opt,"old",'x',&old);
 
   arg=OptionProcess(1,argc,argv,&opt,rst_opterr);
 
@@ -109,7 +109,7 @@ int main (int argc,char *argv[]) {
     exit(-1);
   }
 
-       
+
   if (old) {
     for (c=arg;c<argc;c++) {
       rawfp=OldRawOpen(argv[c],NULL);
@@ -131,12 +131,12 @@ int main (int argc,char *argv[]) {
          fprintf(stdout,"%g",raw->pwr0[i]);
          if ((i % 8)==0) fprintf(stdout,"\n");
          else fprintf(stdout,"\t");
-        
+
        }
        fprintf(stdout,"\n");
-       
-    
-      }    
+
+
+      }
       OldRawClose(rawfp);
     }
   } else {
@@ -163,11 +163,11 @@ int main (int argc,char *argv[]) {
         if ((i % 8)==0) fprintf(stdout,"\n");
         else fprintf(stdout,"\t");
       }
-      fprintf(stdout,"\n");   
+      fprintf(stdout,"\n");
     }
     if (fp !=stdin) fclose(fp);
-  }    
-  
+  }
+
   return 0;
 
 }
