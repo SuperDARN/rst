f47adebd339e8a14950bd6f7d6e1d87610cefdd3
egthomas <evan.g.thomas@dartmouth.edu>
2019-01-25 15:30:27 -0500

    Changing fitscan to check xcf flag rather than if elv is null for loading elevation data

--- codebase/superdarn/src.lib/tk/oldfit.1.25/src/fitscan.c
+++ codebase/superdarn/src.lib/tk/oldfit.1.25/src/fitscan.c
@@ -129,7 +129,7 @@ int OldFitReadRadarScan(struct OldFitFp *fp,int *state,
       bm->rng[r].p_l=fit->rng[r].p_l;
       bm->rng[r].w_l=fit->rng[r].w_l;
       bm->rng[r].v_e=fit->rng[r].v_err;    
-      if (fit->elv!=NULL) bm->rng[r].elv=fit->elv[r].normal;
+      if (prm->xcf !=0) bm->rng[r].elv=fit->elv[r].normal;
       else bm->rng[r].elv=0;
     }
    
