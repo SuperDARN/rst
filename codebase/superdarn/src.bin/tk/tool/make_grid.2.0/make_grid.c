35d44d8b27613163077a4dc76bd36d205fb7a5e5
Angeline G. Burrell <angeline.burrell@nrl.navy.mil>
2019-06-14 08:58:00 -0400

    commented subroutine
    Commented the exclude_outofscan subroutine.

--- codebase/superdarn/src.bin/tk/tool/make_grid.2.0/make_grid.c
+++ codebase/superdarn/src.bin/tk/tool/make_grid.2.0/make_grid.c
@@ -102,6 +102,59 @@ struct GridTable *grid;
 
 
 /**
+ * Exclude beams that are not part of a scan
+ **/
+int exclude_outofscan(struct RadarScan *ptr) {
+
+    int n,num=0;
+    struct RadarBeam *tmp;
+
+    /* Exit if there is no data in this radar scan */
+    if (ptr==NULL) return -1;
+    if (ptr->num==0) return -1;
+
+    /* Initialize a temporary structure, exiting if it ends up being null */
+    /* AGB: should this be able to happen, given the previous two checks? */
+    tmp=malloc(sizeof(struct RadarBeam)*ptr->num);
+    if (tmp==NULL) return -1;
+
+    /* Cycle through each beam in this scan */
+    for (n=0;n<ptr->num;n++) {
+
+      /* Exit if this beam is not part of a scan */
+      if (ptr->bm[n].scan<0) continue;
+
+      /* Save the beam to the temporary structure if it is part of a scan */
+      memcpy(&tmp[num],&ptr->bm[n],sizeof(struct RadarBeam));
+      num++;
+
+    }
+
+    free(ptr->bm);
+
+    /* If there are beams in a scan, assign the temporary data to the radar
+       structure.  Otherwise, merely free the temporary structure */
+    if (num>0) {
+        ptr->bm=realloc(tmp,sizeof(struct RadarBeam)*num);
+        if (ptr->bm==NULL) {
+            free(tmp);
+            ptr->num=0;
+            return -1;
+        }
+    } else {
+        free(tmp);
+        ptr->bm=NULL;
+    }
+
+    /* Update the number of beams in the radar structure */
+    ptr->num=num;
+
+    /* Return success */
+    return 0;
+}
+
+
+/**
  * Exclude scatter in range gates below minrng or beyond maxrng,
  * or from slant ranges below minsrng or beyond maxsrng. If range
  * gate and slant range thresholds are both provided, only the
