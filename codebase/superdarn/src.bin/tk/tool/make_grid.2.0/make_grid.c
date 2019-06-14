5f21122bedbeb2703b3cd292e4d9a53ffde13e86
Angeline G. Burrell <angeline.burrell@nrl.navy.mil>
2019-06-14 11:59:46 -0400

    Update make_grid.c
    Added more descriptions to the subroutine headers

--- codebase/superdarn/src.bin/tk/tool/make_grid.2.0/make_grid.c
+++ codebase/superdarn/src.bin/tk/tool/make_grid.2.0/make_grid.c
@@ -155,10 +155,11 @@ int exclude_outofscan(struct RadarScan *ptr) {
 
 
 /**
- * Exclude scatter in range gates below minrng or beyond maxrng,
- * or from slant ranges below minsrng or beyond maxsrng. If range
- * gate and slant range thresholds are both provided, only the
- * slant range thresholds are considered.
+ * Exclude scatter in range gates below minrng or beyond maxrng (the minimum
+ * and maximum range gates, respectively), or from slant ranges below
+ * minsrng or beyond maxsrng (the minimum and maximum slant ranges,
+ * respectively). If range gate and slant range thresholds are both provided,
+ * only the slant range thresholds are considered.
  **/
 void exclude_range(struct RadarScan *ptr,int minrng,int maxrng,
                    double minsrng,double maxsrng) {
@@ -209,7 +210,7 @@ void exclude_range(struct RadarScan *ptr,int minrng,int maxrng,
 
 
 /**
- *
+ * Parse the comma separated list of beams to exclude
  **/
 void parse_ebeam(char *str) {
 
@@ -234,6 +235,55 @@ void parse_ebeam(char *str) {
 }
 
 
+
+/**
+ * Converts an input date from YYYYMMDD format to an epoch time in number of
+ * seconds since 00:00 UT on January 1, 1970.
+ **/
+double strdate(char *text) {
+
+    double tme;
+    int val;
+    int yr,mo,dy;
+
+    /* Calculate day, month, and year from YYYYMMDD format date */
+    val=atoi(text);
+    dy=val % 100;
+    mo=(val / 100) % 100;
+    yr=(val / 10000);
+
+    /* If only 2-digit year provided then assume it was pre-2000 */
+    if (yr<1970) yr+=1900;
+
+    /* Calculate epoch time of input year, month, and day */
+    tme=TimeYMDHMSToEpoch(yr,mo,dy,0,0,0);
+
+    /* Return epoch time in number of seconds since 00:00UT on January 1, 1970 */
+    return tme;
+
+}
+
+
+/**
+ * Converts an input time from HHMM format to number of seconds.
+ **/
+double strtime(char *text) {
+
+    int hr,mn;
+    int i;
+
+    for (i=0;(text[i] !=':') && (text[i] !=0);i++);
+    if (text[i]==0) return atoi(text)*3600L;
+    text[i]=0;
+    hr=atoi(text);
+    mn=atoi(text+i+1);
+    return hr*3600L+mn*60L;
+
+}
+
+/**
+ * Outputs an error statement for an unrecognized input option
+ **/
 int rst_opterr(char *txt) {
     fprintf(stderr,"Option not recognized: %s\n",txt);
     fprintf(stderr,"Please try: make_grid --help\n");
