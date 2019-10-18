1deb4dd0829b568315645da8e0e4cc7413b3a97b
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2019-10-18 18:01:32 -0400

    updated routines
    Added routines to support new fit structure

--- codebase/superdarn/src.lib/tk/read_fit_v1.0/src/read_fit.c
+++ codebase/superdarn/src.lib/tk/read_fit_v1.0/src/read_fit.c
@@ -1,5 +1,6 @@
 /* read_fitacf.c
-   ===========
+   =============
+   Author: Angeline G. Burrell - NRL - 2019
 */
 
 /*
@@ -24,49 +25,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <ctype.h>
-#include <zlib.h>
-#include "rtypes.h"
-#include "dmap.h"
+/* #include <sys/stat.h>
+ * #include <sys/types.h>
+ * #include <unistd.h>
+ * #include <ctype.h>
+ * #include <zlib.h>
+ * #include "rtypes.h"
+ * #include "dmap.h"
+ */
 #include "option.h"
-#include "rtime.h"
 #include "radar.h"
-#include "rprm.h"
-#include "fitdata.h"
-#include "cfitdata.h"
-#include "scandata.h"
-#include "fitread.h"
-#include "fitscan.h"
-#include "fitindex.h"
-#include "fitseek.h"
-#include "oldfitread.h"
-#include "oldfitscan.h"
-#include "cfitread.h"
-#include "cfitindex.h"
-#include "cfitseek.h"
-#include "cfitscan.h"
-#include "fitscan.h"
-#include "filter.h"
 #include "bound.h"
 #include "checkops.h"
 #include "rpos.h"
+#include "scandata.h"
+#include "multscan.h"
 
 #include "hlpstr.h"
 #include "errstr.h"
-
-struct RadarParm *prm;
-struct FitData *fit;
-struct CFitdata *cfit;
-struct RadarScan *src;
-
-struct FitIndex *inx;
-
-struct RadarNetwork *network;
-struct Radar *radar;
-struct RadarSite *site;
 
 struct OptionData opt;
 
@@ -76,33 +52,28 @@ struct OptionData opt;
 
 int main(int argc, char *argv[])
 {
-  int inum, fnum;
-  int yr, mo, dy, hr, mt;
- 
-  double sc;
+  int len, fnum, channel, channel_fix, ret_stat;
 
-  char vstr[256], *envstr;
-  FILE *fp;
+  char vstr[256];
 
   /* Initialize input options */
-  int s=0, state=0, syncflg=1;
-  int old=0, farg=0, tlen=0, channel=0, channel_fix=-1;
+  int old=0, farg=0, tlen=0;
   
-  double stime=-1, etime=-1, extime=0, sdate=-1, edate=-1;
+  double stime=-1.0, etime=-1.0, extime=0.0, sdate=-1.0, edate=-1.0;
 
   unsigned char cfitflg=0, fitflg=0, vb=0, catflg=0, nsflg=0;
 
-  char *chnstr=NULL, *chnstr_fix=NULL;
+  char *chnstr=NULL, *chnstr_fix=NULL, *exstr=NULL, *vbuf=NULL;
   char *stmestr=NULL, *etmestr=NULL, *sdtestr=NULL, *edtestr=NULL;
-  char *exstr=NULL, *vbuf=NULL;
 
   /* Initialize file information */
-  char *dname=NULL, *iname=NULL;
+  char **dnames=NULL, *iname=NULL;
   FILE *fitfp=NULL;
 
-  /* Initialize structures */
-  struct OldFitFp *oldfitfp=NULL;
-  struct CFitfp *cfitfp=NULL;
+  /* Initialize fit scan structure TESTING ONLY */
+  struct MultRadarScan *mult_scans;
+  void write_mult_scan(FILE *fp, struct MultRadarScan *mult_scan,
+		       unsigned char vb, char *vbuf);
 
   /* Declare local subroutines */
   int command_options(int argc, char *argv[], int *old, int *tlen,
@@ -111,374 +82,84 @@ int main(int argc, char *argv[])
 		      unsigned char *nsflg, char *stmestr, char *etmestr,
 		      char *sdtestr, char *edtestr, char *exstr, char *chnstr,
 		      char *chnstr_fix);
+  int set_stereo_channel(char *chnstr);
+  int set_fix_channel(char *chnstr_fix);
   double strtime(char *text);
   int exclude_outofscan(struct RadarScan *ptr);
   double strdate(char *text);
-  void write_scan(FILE *fp, struct RadarScan *scan, unsigned char vb,
-		  char *vbuf);
-
+  int load_fit(int fnum, int channel, int channel_fix, int old, int tlen,
+	       double stime, double sdate, double etime, double edate,
+	       unsigned char cfitflg, unsigned char fitflg, unsigned char nsflg,
+	       char *iname, char *dnames, char *vbuf);
+  
   /* Process the command line options */
   farg = command_options(argc, argv, &old, &tlen, &vb, &cfitflg, &fitflg,
 			 &catflg, &nsflg, stmestr, etmestr, sdtestr,
 			 edtestr, exstr, chnstr, chnstr_fix);
 
-  /* Initialize radar parameter and fit/cfit structures */
-  prm = RadarParmMake();
-  fit = FitMake();
-  cfit = CFitMake();
-
-  /* Initialize RadarScan structure */
-  src = RadarScanMake();
-
-  /* Write the header line */
-  write_scan(stdout, NULL, vb, vbuf);
-
-  /* Make sure the SD_RADAR environment variable is set */
-  envstr=getenv("SD_RADAR");
-  if(envstr==NULL)
-    {
-      fprintf(stderr, "Environment variable 'SD_RADAR' must be defined.\n");
-      exit(-1);
-    }
-
-  /* Open the radar information file */
-  fp = fopen(envstr, "r");
-  if(fp==NULL)
-    {
-      fprintf(stderr,"Could not locate radar information file.\n");
-      exit(-1);
-    }
-
-  /* Load the radar network information */
-  network = RadarLoad(fp);
-  fclose(fp);
-  if(network==NULL)
-    {
-      fprintf(stderr,"Failed to read radar information.\n");
-      exit(-1);
-    }
-
-  /* Make sure the SD_HDWPATH environment variable is set */
-  envstr = getenv("SD_HDWPATH");
-  if(envstr==NULL)
-    {
-      fprintf(stderr,"Environment variable 'SD_HDWPATH' must be defined.\n");
-      exit(-1);
-    }
-
-  /* Load the hardware information for the radar network */
-  RadarLoadHardware(envstr, network);
-
   /* If 'cn' set then determine Stereo channel, either A or B */
-  if(chnstr != NULL)
-    {
-      if(tolower(chnstr[0]) == 'a') channel = 1;
-      if(tolower(chnstr[0]) == 'b') channel = 2;
-    }
+  channel = set_stereo_channel(chnstr);
     
   /* If 'cn_fix' set then determine appropriate channel for output file */
-  if(chnstr_fix != NULL)
-    {
-      if(tolower(chnstr_fix[0]) == 'a') channel_fix=1;
-      if(tolower(chnstr_fix[0]) == 'b') channel_fix=2;
-      if(tolower(chnstr_fix[0]) == 'c') channel_fix=3;
-      if(tolower(chnstr_fix[0]) == 'd') channel_fix=4;
-    }
+  channel_fix = set_fix_channel(chnstr_fix);
 
   /* Format the time data */
-  if (exstr != NULL)  extime = strtime(exstr);
-  if (stmestr != NULL) stime = strtime(stmestr);
-  if (etmestr != NULL) etime = strtime(etmestr);
-  if (sdtestr != NULL) sdate = strdate(sdtestr);
-  if (edtestr != NULL) edate = strdate(edtestr);
+  if(exstr != NULL)  extime = strtime(exstr);
+  if(stmestr != NULL) stime = strtime(stmestr);
+  if(etmestr != NULL) etime = strtime(etmestr);
+  if(sdtestr != NULL) sdate = strdate(sdtestr);
+  if(edtestr != NULL) edate = strdate(edtestr);
 
   /* If verbose, set output */
   if(vb) vbuf = vstr;
 
-  /* Determine the file name indeces */
-  inum = -1;
+  /* Make a list of input files */
   if(catflg == 0)
     {
       /* For a single input file, an index file may also be provided */
-      if(argc - farg > 1)
+      dnames = (char **)malloc(sizeof(*char));
+
+      if(argc-farg > 1)
 	{
+	  iname = argv[argc-1];
 	  fnum = argc - 2;
-	  inum = argc - 1;
 	}
-      else
-	fnum = argc - 1;
+      else fnum = argc - 1;
+
+      len = strlen(argv[fnum]);
+      dnames[0] = (char *)malloc(sizeof(char) * (len+1));
+      strcpy(dnames[0], argv[fnum]);
+      dnames[0][len] = \0;
+      fnum = 1;
     }
   else
     {
       /* For multiple input files, no index files are allowed */
-      fnum = farg;
+      fnum = argc - farg;
+      dnames = (char **)malloc(sizeof(*char) * fnum);
+
+      for(inum=0; inum<fnum; inum++)
+	{
+	  len = strlen(argv[inum+argc]);
+	  dnames[inum] = (char *)malloc(sizeof(char) * (len+1));
+	  strcpy(dnames[inum], argv[inum+argc]);
+	  dnames[inum][len] = \0;
+	}
     }
 
-  for(; fnum < argc; fnum++)
-    {
-      /* Assign the input and index file names */
-      dname = argv[fnum];
-      if(inum >= 0) iname = argv[inum];
+  /* TEST ONLY */
+  mult_scans = (struct MultRadarScan *)(malloc(sizeof(struct MultRadarScan)));
+  memset(mult_scans, 0, sizeof(struct MultRadarScan))
 
-      fprintf(stderr, "Opening file:%s\n", argv[fnum]);
+  /* Put the load routine here */
+  ret_stat = load_fit(fnum, channel, channel_fix, old, tlen, stime, sdate,
+		      etime, edate, cfitflg, fitflg, nsflg, iname, dnames,
+		      vbuf, mult_scans);
 
-      /* Open the fit, fitacf, or cfit file and read the first scan */
-      if(fitflg)
-	{
-	  /* Input file is in fit or fitacf format */
-	  if(old)
-	    {
-	      /* Input file is in fit format */
+  /* TEST WRITE */
+  write_mult_scan(stdout, mult_scans, vb, vbuf);
 
-	      /* Open the fit file for reading */
-	      if((oldfitfp = OldFitOpen(dname, iname)) == NULL)
-		{
-		  fprintf(stderr,"File not found.\n");
-		  exit(-1);
-                }
-
-	      /* Read first available radar scan in fit file (will use scan
-		 flag if tlen not provided) */
-	      s = OldFitReadRadarScan(oldfitfp, &state, src, prm,
-                                      fit, tlen, syncflg, channel);
-
-	      /* Verify that scan was properly read */
-	      if(s == -1)
-		{
-		  fprintf(stderr,"Error reading file.\n");
-		  exit(-1);
-                }
-            }
-	  else
-	    {
-	      /* Input file is in fitacf format */
-
-	      /* Check if index file provided */
-	      if(iname != NULL)
-		{
-		  /* Open the index file for reading */
-		  if((fp = fopen(iname, "r")) == NULL)
-		    {
-		      fprintf(stderr,"Could not open index.\n");
-                    }
-		  else
-		    {
-		      if((inx = FitIndexFload(fp)) == NULL)
-			fprintf(stderr,"Error loading index.\n");
-		      fclose(fp);
-                    }
-                }
-
-	      /* Open the fitacf file for reading */
-	      if((fitfp=fopen(dname,"r")) == NULL)
-		{
-		  fprintf(stderr,"File not found.\n");
-		  exit(-1);
-                }
-
-	      /* Read first available radar scan in fitacf file (will use scan
-		 flag if tlen not provided) */
-	      s = FitFreadRadarScan(fitfp, &state, src, prm, fit,
-                                    tlen, syncflg, channel);
-
-	      /* Verify that scan was properly read */
-	      if(s == -1)
-		{
-		  fprintf(stderr,"Error reading file.\n");
-		  exit(-1);
-                }
-            }
-        }
-      else
-	{
-	  /* Input file is in cfit or format */
-
-	  /* Open the cfit file for reading */
-	  if((cfitfp=CFitOpen(dname)) == NULL)
-	    {
-	      fprintf(stderr,"File not found.\n");
-	      exit(-1);
-            }
-
-	  /* Verify that the cfit file was properly opened */
-	  s = CFitReadRadarScan(cfitfp, &state, src, cfit, tlen, syncflg,
-				channel);
-	  if(s == -1)
-	    {
-	      fprintf(stderr,"Error reading file.\n");
-	      exit(-1);
-            }
-        }
-
-      /* If either start time or date not provided as input then determine it */
-      if((stime != -1) || (sdate != -1))
-	{
-	  /* we must skip the start of the files */
-
-	  /* If start time not provided then use time of first record
-	   * in fit file */
-	  if(stime==-1) stime = ((int) src->st_time % (24*3600));
-
-	  /* If start date not provided then use date of first record
-	   * in fit file, otherwise use provided sdate */
-	  if (sdate==-1) stime += src->st_time -
-			   ((int) src->st_time % (24*3600));
-	  else stime += sdate;
-
-	  /* Calculate year, month, day, hour, minute, and second of 
-	     grid start time */
-	  TimeEpochToYMDHMS(stime, &yr, &mo, &dy, &hr, &mt, &sc);
-
-	  /* Search for index of corresponding record in input file given
-	     grid start time */
-	  if(fitflg)
-	    {
-	      /* Input file is in fit or fitacf format */
-
-	      if(old) s = OldFitSeek(oldfitfp, yr, mo, dy, hr, mt, sc, NULL);
-	      else s = FitFseek(fitfp, yr, mo, dy, hr, mt, sc, NULL, inx);
-
-	      /* If a matching record could not be found then exit */
-	      if(s == -1)
-		{
-		  fprintf(stderr,
-			  "File does not contain the requested interval.\n");
-		  exit(-1);
-                }
-
-	      /* If using scan flag instead of tlen then continue to read
-		 fit records until reaching the beginning of a new scan */
-	      if(tlen==0)
-		{
-		  if(old)
-		    {
-		      while((s=OldFitRead(oldfitfp,prm,fit)) != -1)
-			{
-			  if(abs(prm->scan) == 1) break;
-                        }
-                    }
-		  else
-		    {
-		      while((s=FitFread(fitfp,prm,fit)) != -1)
-			{
-			  if(abs(prm->scan) == 1) break;
-                        }
-                    }
-                }
-	      else state=0;
-
-	      /* Read the first full scan of data from open fit or fitacf file
-		 corresponding to grid start date and time */
-	      if(old) s = OldFitReadRadarScan(oldfitfp, &state, src, prm,
-					      fit, tlen, syncflg, channel);
-	      else s = FitFreadRadarScan(fitfp, &state, src, prm, fit,
-					 tlen, syncflg, channel);
-            }
-	  else
-	    {
-	      /* Input file is in cfit format */
-
-	      s = CFitSeek(cfitfp, yr, mo, dy, hr, mt, sc, NULL, NULL);
-
-	      /* If a matching record could not be found then exit */
-	      if(s == -1)
-		{
-		  fprintf(stderr,
-			  "File does not contain the requested interval.\n");
-		  exit(-1);
-                }
-
-	      /* If using scan flag instead of tlen then continue to read
-		 cfit records until reaching the beginning of a new scan */
-	      if(tlen==0)
-		{
-		  while((s=CFitRead(cfitfp,cfit)) != -1)
-		    {
-		      if(cfit->scan == 1) break;
-                    }
-                }
-	      else state = 0;
-
-	      /* Read the first full scan of data from open cfit file
-		 corresponding to the start date and time */
-	      s = CFitReadRadarScan(cfitfp, &state, src, cfit, tlen,
-				    syncflg, channel);
-
-            }
-        }
-      else stime = src->st_time;   /* If the start date and time are not
-				      provided, use the time of the input
-				      file's first record */
-
-      /* If end time provided then determine end date */
-      if(etime != -1)
-	{
-	  /* If end date not provided then use date of first record
-	     in input file */
-	  if(edate == -1) etime += src->st_time -
-                            ((int) src->st_time % (24*3600));
-	  else etime += edate;
-        }
-
-      /* If time extent provided then use that to calculate end time */
-      if(extime != 0) etime = stime + extime;
-
-      /* Continue writing data to stdout until the input scan data is beyond
-	 the end time or end of input file is reached */
-      do
-	{
-	  /* If 'ns' option set then exclude data where scan flag = -1 */
-	  if(nsflg) exclude_outofscan(src);
-
-	  /* Calculate year, month, day, hour, minute and second of the output
-	     scan start time */
-	  TimeEpochToYMDHMS(src->st_time,&yr,&mo,&dy,&hr,&mt,&sc);
-
-	  /* Load the appropriate radar hardware information for the day
-	     and time of the radar scan (only done once) */
-	  if(site == NULL)
-	    {
-	      radar = RadarGetRadar(network, src->stid);
-	      if(radar==NULL)
-		{
-		  fprintf(stderr,"Failed to get radar information.\n");
-		  exit(-1);
-		}
-	      site = RadarYMDHMSGetSite(radar, yr, mo, dy, hr, mt, (int)sc);
-	    }
-
-	  /* write data to output file */
-	  write_scan(stdout, src, vb, vbuf);
-
-	  if(vbuf != NULL) fprintf(stderr,"Storing:%s\n", vbuf);
-
-	  /* Read next radar scan from input file */
-	  if(fitflg)
-	    {
-	      if(old) s = OldFitReadRadarScan(oldfitfp, &state, src, prm, fit,
-					      tlen, syncflg, channel);
-	      else s = FitFreadRadarScan(fitfp, &state, src, prm, fit,
-					 tlen, syncflg, channel);
-            }
-	  else
-	    s = CFitReadRadarScan(cfitfp, &state, src, cfit, tlen, syncflg,
-				  channel);
-
-	  /* If scan data is beyond end time then break out of loop */
-	  if((etime != -1) && (src->st_time>etime)) break;
-	  
-        } while (s != -1);
-
-      /* Close the input file */
-      if(fitflg)
-	{
-	  if(old) OldFitClose(oldfitfp);
-	  else fclose(fitfp);
-        }
-      else CFitClose(cfitfp);
-    }
-
-    return 0;
+  return ret_stat;
 
 }
 
