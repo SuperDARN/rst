3b706f4f6b39fe66609e62f5e6f2f62ca717119c
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2019-10-18 18:01:32 -0400

    updated routines
    Added routines to support new fit structure

---
+++ codebase/superdarn/src.lib/tk/read_fit_v1.0/src/read_fit_funcs.c
@@ -0,0 +1,59 @@
+/* read_fit_funcs.c
+   ===================
+   Author: Angeline G. Burrell - NRL - 2019
+*/
+
+/*
+ LICENSE AND DISCLAIMER
+
+ This file is part of the Radar Software Toolkit (RST).
+
+ RST is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ any later version.
+
+ RST is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with RST.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int set_stereo_channel(char *chnstr)
+{
+  int channel = 0;
+
+  if(chnstr != NULL)
+    {
+      if(tolower(chnstr[0]) == 'a') channel = 1;
+      else if(tolower(chnstr[0]) == 'b') channel = 2;
+      else
+	printf(stderr, "Unknown Stereo channel %s, leaving unset\n", chnstr);
+    }
+
+  return(channel);
+}
+
+int set_fix_channel(char *chnstr_fix)
+{
+  int channel_fix=-1;
+
+  if(chnstr_fix != NULL)
+    {
+      if(tolower(chnstr_fix[0]) == 'a') channel_fix = 1;
+      else if(tolower(chnstr_fix[0]) == 'b') channel_fix = 2;
+      else if(tolower(chnstr_fix[0]) == 'c') channel_fix = 3;
+      else if(tolower(chnstr_fix[0]) == 'd') channel_fix = 4;
+      else
+	printf(stderr, "Unknown fix channel %s, leaving unset\n", chnstr_fix);
+    }
+
+  return(channel_fix);
+}
