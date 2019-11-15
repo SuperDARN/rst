db07df197a24fd128bff1af8c5a16ddf84af867b
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2019-11-15 11:48:39 -0500

    ENH: Added make_fov
    Added the outline of the FoV routine.  To avoid adding more data structures
    than necessary, included all of the data and flags that will be needed for the
    backscatter ID code as well.

---
+++ codebase/superdarn/src.bin/tk/tool/make_fov.1.0/src/make_fov.c
@@ -0,0 +1,252 @@
+/* make_fov.c
+   =============
+   Author: Angeline G. Burrell - NRL - 2019
+*/
+
+/*
+ LICENSE AND DISCLAIMER
+
+ This file is part of the Radar Software Toolkit (RST).
+
+ RST is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ any later version.
+
+ RST is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with RST.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "option.h"
+#include "radar.h"
+#include "rpos.h"
+#include "scandata.h"
+#include "multscan.h"
+#include "multbsid.h"
+#include "hlpstr.h"
+#include "errstr.h"
+
+struct OptionData opt;
+
+/**
+ * Determines the backscatter origin field-of-view
+ **/
+
+int main(int argc, char *argv[])
+{
+  int inum, len, fnum, channel, channel_fix, ret_stat;
+
+  char vstr[256];
+
+  struct MultRadarScan *mult_scan;
+  struct MultRadarBSID *mult_bsid;
+
+  /* Initialize input options */
+  int old=0, farg=0, tlen=0;
+  
+  double stime=-1.0, etime=-1.0, extime=0.0, sdate=-1.0, edate=-1.0;
+
+  unsigned char cfitflg=0, fitflg=0, vb=0, catflg=0, nsflg=0;
+
+  char *chnstr=NULL, *chnstr_fix=NULL, *exstr=NULL, *vbuf=NULL;
+  char *stmestr=NULL, *etmestr=NULL, *sdtestr=NULL, *edtestr=NULL;
+
+  /* Initialize file information */
+  char **dnames=NULL, *iname=NULL;
+
+  /* Declare local subroutines */
+  int command_options(int argc, char *argv[], int *old, int *tlen,
+		      unsigned char *vb, unsigned char *cfitflg,
+		      unsigned char *fitflg, unsigned char *catflg,
+		      unsigned char *nsflg, char *stmestr, char *etmestr,
+		      char *sdtestr, char *edtestr, char *exstr, char *chnstr,
+		      char *chnstr_fix);
+  int set_stereo_channel(char *chnstr);
+  int set_fix_channel(char *chnstr_fix);
+  double strtime(char *text);
+  double strdate(char *text);
+  int load_fit(int fnum, int channel, int channel_fix, int old, int tlen,
+	       double stime, double sdate, double etime, double edate,
+	       double extime, unsigned char wrtflg, unsigned char cfitflg,
+	       unsigned char fitflg, unsigned char nsflg, unsigned char vb,
+	       char *vbuf, char *iname, char **dnames,
+	       struct MultRadarScan *mult_scan);
+  
+  /* Process the command line options */
+  farg = command_options(argc, argv, &old, &tlen, &vb, &cfitflg, &fitflg,
+			 &catflg, &nsflg, stmestr, etmestr, sdtestr,
+			 edtestr, exstr, chnstr, chnstr_fix);
+
+  /* If 'cn' set then determine Stereo channel, either A or B */
+  channel = set_stereo_channel(chnstr);
+    
+  /* If 'cn_fix' set then determine appropriate channel for output file */
+  channel_fix = set_fix_channel(chnstr_fix);
+
+  /* Format the time data */
+  if(exstr != NULL)  extime = strtime(exstr);
+  if(stmestr != NULL) stime = strtime(stmestr);
+  if(etmestr != NULL) etime = strtime(etmestr);
+  if(sdtestr != NULL) sdate = strdate(sdtestr);
+  if(edtestr != NULL) edate = strdate(edtestr);
+
+  /* If verbose, set output */
+  if(vb) vbuf = vstr;
+
+  /* Make a list of input files */
+  if(catflg == 0)
+    {
+      /* For a single input file, an index file may also be provided */
+      dnames = (char **)malloc(sizeof(char*));
+
+      if(argc-farg > 1)
+	{
+	  iname = argv[argc-1];
+	  fnum = argc - 2;
+	}
+      else fnum = argc - 1;
+
+      len = strlen(argv[fnum]);
+      dnames[0] = (char *)malloc(sizeof(char) * (len+1));
+      strcpy(dnames[0], argv[fnum]);
+      fnum = 1;
+    }
+  else
+    {
+      /* For multiple input files, no index files are allowed */
+      fnum = argc - farg;
+      dnames = (char **)malloc(sizeof(char*) * fnum);
+
+      for(inum=0; inum<fnum; inum++)
+	{
+	  len = strlen(argv[inum+argc]);
+	  dnames[inum] = (char *)malloc(sizeof(char) * (len+1));
+	  strcpy(dnames[inum], argv[inum+argc]);
+	}
+    }
+
+  /* Initialize and load the fitted data here */
+  mult_scan = (struct MultRadarScan *mult_scan)
+    (malloc(sizeof(struct MultRadarScan)));
+  memset(mult_scan, 0, sizeof(struct MultRadarScan));
+  mult_scan->num_scans = 0;
+  ret_stat = load_fit(fnum, channel, channel_fix, old, tlen, stime, sdate,
+		      etime, edate, extime, 0, cfitflg, fitflg, nsflg, vb, vbuf,
+		      iname, dnames, mult_scan);
+
+  /* Cycle through the scans, updating the backscatter data in each one */
+  /* Based off of DaViTpy routine:                                      */
+  /*   pydarn.proc.fov.update_backscatter.update_bs_w_scan              */
+  ret_stat = update_scan_bs(min_pnts, region_hmax, region_hmin, rg_box, vh_box,
+			    max_rg, max_hop, ptst, strict_gs, step, mult_scan,
+			    mult_bsid);
+
+  /* Dynamically establish transmission frequency bands for these scans */
+  ret_stat = get_radar_tfreq_bands(band_width, mult_scan, fbands);
+
+  /* Examine the UT evolution and consistency of the elevation angles */
+  ret_stat = test_ut_fov_struct(min_frac, frg_box, max_rg, ut_box, fbands, step,
+				mult_bsid);
+
+  return ret_stat;
+}
+
+
+/**
+ * Load the command line options
+ **/
+
+int command_options(int argc, char *argv[], int *old, int *tlen,
+		    unsigned char *vb, unsigned char *cfitflg,
+		    unsigned char *fitflg, unsigned char *catflg,
+		    unsigned char *nsflg, char *stmestr,
+		    char *etmestr, char *sdtestr, char *edtestr, char *exstr,
+		    char *chnstr, char *chnstr_fix)
+{
+  /* Initialize input options */
+  int farg=0;
+  unsigned char help=0, option=0, version=0;
+
+  int rst_opterr(char *txt);
+
+
+  /* If only information is desired, print it out and exit */
+  OptionAdd(&opt, "-help", 'x', &help);
+  OptionAdd(&opt, "-option", 'x', &option);
+  OptionAdd(&opt, "-version", 'x', &version);
+
+  /* If 'help' set then print help message */
+  if(help==1)
+    {
+      OptionPrintInfo(stdout, hlpstr);
+      exit(0);
+    }
+
+  /* If 'option' set then print all command line options */
+  if(option==1)
+    {
+      OptionDump(stdout, &opt);
+      exit(0);
+    }
+
+  /* If 'version' set, then print the version number */
+  if(version==1)
+    {
+      OptionVersion(stdout);
+      exit(0);
+    }
+
+  /* Determine input file format */
+  OptionAdd(&opt, "old", 'x', old);      /* Old fit format */
+  OptionAdd(&opt, "fit", 'x', fitflg);   /* New fit format */
+  OptionAdd(&opt, "cfit", 'x', cfitflg); /* cfit format    */
+
+  /* If not explicity working from cfit files, treat input as fit or fitacf */
+  if(*cfitflg == 0) *fitflg = 1;
+
+  /* Concatenate multiple input files */
+  OptionAdd(&opt, "c", 'x', catflg); 
+
+  /* Verbose: log information to console */
+  OptionAdd(&opt, "vb", 'x', vb);
+
+  /* Set time information */
+  OptionAdd(&opt, "st", 't', stmestr);  /* Start time in HH:MM format */
+  OptionAdd(&opt, "et", 't', etmestr);  /* End time in HH:MM format */
+  OptionAdd(&opt, "sd", 't', sdtestr);  /* Start date in YYYYMMDD format */
+  OptionAdd(&opt, "ed", 't', edtestr);  /* End date in YYYYMMDD format */
+  OptionAdd(&opt, "ex", 't', exstr);    /* Use interval with extent HH:MM */
+
+  /* Ignore scan flag, use scan length of tl seconds */
+  OptionAdd(&opt, "tl", 'i', tlen);
+
+  /* Process channel options */
+  OptionAdd(&opt, "cn", 't', chnstr);         /* For stereo channel a or b   */
+  OptionAdd(&opt, "cn_fix", 't', chnstr_fix); /* User-defined channel number */
+
+  /* Apply scan flag limit (ie exclude data with scan flag = -1) */
+  OptionAdd(&opt, "ns", 'x', &nsflg);
+
+  /* Process command line options */
+  farg = OptionProcess(1, argc, argv, &opt, rst_opterr);
+
+  /* If command line option not recognized then print error and exit */
+  if(farg == -1) exit(-1);
+
+  if(farg == argc)
+    {
+      OptionPrintInfo(stderr, errstr);
+      exit(-1);
+    }
+
+  return farg;
+}
