503277111ea1de9d83e9e88b43db8b5841deafa2
egthomas <evan.g.thomas@dartmouth.edu>
2019-12-31 10:14:11 -0500

    Adding updated AACGM_v2 coefficient files and modifying environment variables to use new filenames

--- .profile/superdarn.bash
+++ .profile/superdarn.bash
@@ -10,8 +10,8 @@ export AACGM_DAT_PREFIX=${RSTPATH}"/tables/analysis/aacgm/aacgm_coeffs"
 export IGRF_PATH=${RSTPATH}"/tables/analysis/mag/"
 export SD_MODEL_TABLE=${RSTPATH}"/tables/superdarn/model"
 
-export AACGM_v2_DAT_PREFIX="${RSTPATH}/tables/analysis/aacgm/aacgm_coeffs-12-"
-export IGRF_COEFFS="${IGRF_PATH}magmodel_1590-2015.txt"
+export AACGM_v2_DAT_PREFIX="${RSTPATH}/tables/analysis/aacgm/aacgm_coeffs-13-"
+export IGRF_COEFFS="${IGRF_PATH}magmodel_1590-2020.txt"
 
 export COLOR_TABLE_PATH=${RSTPATH}"/tables/base/key/"
 
