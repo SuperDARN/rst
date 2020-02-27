ed984fcc0bcc13736fb599b8fd52e6ef64e75398
egthomas <evan.g.thomas@dartmouth.edu>
2020-02-27 10:02:16 -0500

    Cleaning up spacing in trim_iq

--- codebase/superdarn/src.bin/tk/tool/trim_iq.1.5/trim_iq.c
+++ codebase/superdarn/src.bin/tk/tool/trim_iq.1.5/trim_iq.c
@@ -50,15 +50,11 @@
 #include "errstr.h"
 #include "hlpstr.h"
 
-
-
-
 struct RadarParm *prm=NULL;
 struct IQ *iq=NULL;
 struct IQIndex *inx=NULL;
 unsigned int *badtr=NULL;
 int16 *samples=NULL;
-
 
 struct OptionData opt;
 
@@ -70,7 +66,7 @@ double strdate(char *text) {
   dy=val % 100;
   mo=(val / 100) % 100;
   yr=(val / 10000);
-  if (yr<1970) yr+=1900;  
+  if (yr<1970) yr+=1900;
   tme=TimeYMDHMSToEpoch(yr,mo,dy,0,0,0);
 
   return tme;
@@ -93,7 +89,7 @@ double strtime(char *text) {
   mn=atoi(text+i+1);
   sc=atof(text+j+1);
   return (double) hr*3600L+mn*60L+sc;
-}   
+}
 
 int rst_opterr(char *txt) {
   fprintf(stderr,"Option not recognized: %s\n",txt);
@@ -104,7 +100,6 @@ int rst_opterr(char *txt) {
 int main (int argc,char *argv[]) {
 
   int arg=0;
-
 
   int status=0;
   double atime;
@@ -129,7 +124,6 @@ int main (int argc,char *argv[]) {
   unsigned char option=0;
   unsigned char version=0;
 
-
   FILE *fp=NULL;
 
   int n;
@@ -140,8 +134,6 @@ int main (int argc,char *argv[]) {
   OptionAdd(&opt,"-help",'x',&help);
   OptionAdd(&opt,"-option",'x',&option);
   OptionAdd(&opt,"-version",'x',&version);
-
-
   OptionAdd(&opt,"vb",'x',&vb);
   OptionAdd(&opt,"st",'t',&stmestr);
   OptionAdd(&opt,"et",'t',&etmestr);
@@ -176,8 +168,6 @@ int main (int argc,char *argv[]) {
   if (sdtestr !=NULL) sdate=strdate(sdtestr);
   if (edtestr !=NULL) edate=strdate(edtestr);
 
-
-
   if ((argc-arg)>1) {
     fp=fopen(argv[arg+1],"r");
     if (fp==NULL) {
@@ -185,7 +175,7 @@ int main (int argc,char *argv[]) {
       exit(-1);
     }
     inx=IQIndexFload(fp);
-   
+
     fclose(fp);
     if (inx==NULL) {
       fprintf(stderr,"Error loading index.\n");
@@ -198,105 +188,77 @@ int main (int argc,char *argv[]) {
     fprintf(stderr,"File not found.\n");
     exit(-1);
   }
-  
 
-  if (IQFread(fp,prm,iq,&badtr,&samples)==-1)  {
+  if (IQFread(fp,prm,iq,&badtr,&samples)==-1) {
     fprintf(stderr,"Error reading file\n");
     exit(-1);
   }
 
-  atime=TimeYMDHMSToEpoch(prm->time.yr,
-	   	           prm->time.mo,
-                           prm->time.dy,
-                           prm->time.hr,
-		           prm->time.mt,
-                           prm->time.sc+prm->time.us/1.0e6);
+  atime=TimeYMDHMSToEpoch(prm->time.yr,prm->time.mo,prm->time.dy,
+                          prm->time.hr,prm->time.mt,
+                          prm->time.sc+prm->time.us/1.0e6);
 
    /* skip here */
 
-    if ((stime !=-1) || (sdate !=-1)) { 
-      /* we must skip the start of the files */
-      int yr,mo,dy,hr,mt;
-      double sc; 
-      if (stime==-1) stime= ( (int) atime % (24*3600));
-      if (sdate==-1) stime+=atime - ( (int) atime % (24*3600));
-      else stime+=sdate;
+  if ((stime !=-1) || (sdate !=-1)) {
+    /* we must skip the start of the files */
+    int yr,mo,dy,hr,mt;
+    double sc;
+    if (stime==-1) stime= ( (int) atime % (24*3600));
+    if (sdate==-1) stime+=atime - ( (int) atime % (24*3600));
+    else stime+=sdate;
 
-      TimeEpochToYMDHMS(stime,&yr,&mo,&dy,&hr,&mt,&sc);
-      status=IQFseek(fp,yr,mo,dy,hr,mt,sc,NULL,inx); 
-    
-      if (status ==-1) {
-        fprintf(stderr,"File does not contain the requested interval.\n");
-        exit(-1);
-      }
-      if (IQFread(fp,prm,iq,&badtr,&samples)==-1)  {
-         fprintf(stderr,"Error reading file\n");
-         exit(-1);
-      }
-  
-    } else stime=atime;
-   
-    if (etime !=-1) {
-       if (edate==-1) etime+=atime - ( (int) atime % (24*3600));
-       else etime+=edate;
+    TimeEpochToYMDHMS(stime,&yr,&mo,&dy,&hr,&mt,&sc);
+    status=IQFseek(fp,yr,mo,dy,hr,mt,sc,NULL,inx);
+
+    if (status ==-1) {
+      fprintf(stderr,"File does not contain the requested interval.\n");
+      exit(-1);
     }
 
-   if (extime !=0) etime=stime+extime;
- 
-   do {
+    if (IQFread(fp,prm,iq,&badtr,&samples)==-1) {
+      fprintf(stderr,"Error reading file\n");
+      exit(-1);
+    }
+  } else stime=atime;
 
-     atime=TimeYMDHMSToEpoch(prm->time.yr,prm->time.mo,prm->time.dy,
-                             prm->time.hr,prm->time.mt,
-                             prm->time.sc+prm->time.us/1.0e6);
+  if (etime !=-1) {
+    if (edate==-1) etime+=atime - ( (int) atime % (24*3600));
+    else etime+=edate;
+  }
 
-     if ((etime !=-1) && (atime>=etime)) break;
+  if (extime !=0) etime=stime+extime;
 
-     fprintf(stderr,"%s\n",prm->origin.time);
-     fprintf(stderr,"%s\n",prm->origin.command);
-     fprintf(stderr,"%s\n",prm->combf);
-     fprintf(stderr,"%d:",prm->mppul);
-     for (n=0;n<prm->mppul;n++) fprintf(stderr,"%d ",prm->pulse[n]);
-     fprintf(stderr,"\n");
-     fprintf(stderr,"%d\n",prm->mplgs);
-     for (n=0;n<=prm->mplgs;n++) fprintf(stderr,"%d,%d ",
-          prm->lag[0][n],prm->lag[1][n]);
-     fprintf(stderr,"\n");
+  do {
 
+    atime=TimeYMDHMSToEpoch(prm->time.yr,prm->time.mo,prm->time.dy,
+                            prm->time.hr,prm->time.mt,
+                            prm->time.sc+prm->time.us/1.0e6);
 
-         
-     IQFwrite(stdout,prm,iq,badtr,samples);
-     
-     TimeEpochToYMDHMS(atime,&yr,&mo,&dy,&hr,&mt,&sc);
-     if (vb==1) fprintf(stderr,"%d-%d-%d %d:%d:%d\n",yr,mo,dy,hr,mt,(int) sc);
+    if ((etime !=-1) && (atime>=etime)) break;
 
-      status=IQFread(fp,prm,iq,&badtr,&samples);
- 
-  } while (status !=-1);  
- 
+    TimeEpochToYMDHMS(atime,&yr,&mo,&dy,&hr,&mt,&sc);
+    if (vb==1) fprintf(stderr,"%d-%d-%d %d:%d:%d\n",yr,mo,dy,hr,mt,(int) sc);
+
+    fprintf(stderr,"%s\n",prm->origin.time);
+    fprintf(stderr,"%s\n",prm->origin.command);
+    fprintf(stderr,"%s\n",prm->combf);
+    fprintf(stderr,"%d:",prm->mppul);
+    for (n=0;n<prm->mppul;n++) fprintf(stderr,"%d ",prm->pulse[n]);
+    fprintf(stderr,"\n");
+    fprintf(stderr,"%d\n",prm->mplgs);
+    for (n=0;n<=prm->mplgs;n++) fprintf(stderr,"%d,%d ",prm->lag[0][n],prm->lag[1][n]);
+    fprintf(stderr,"\n");
+
+    IQFwrite(stdout,prm,iq,badtr,samples);
+
+    status=IQFread(fp,prm,iq,&badtr,&samples);
+
+  } while (status !=-1);
+
   if (fp !=stdin) fclose(fp);
+
   return 0;
 
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
