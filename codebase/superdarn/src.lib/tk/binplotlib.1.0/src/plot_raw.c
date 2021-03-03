a37dc5dd29c290b656c066b5c5365c02d0256cca
mts299 <marina.t.schmidt@gmail.com>
2021-03-03 11:39:02 -0600

    removed see license.txt and replace with GPL disclaimer

--- codebase/superdarn/src.lib/tk/binplotlib.1.0/src/plot_raw.c
+++ codebase/superdarn/src.lib/tk/binplotlib.1.0/src/plot_raw.c
@@ -1,13 +1,23 @@
 /* plot_raw.c
    ========== 
    Author: R.J.Barnes and others
-*/
 
-/*
-   See license.txt
-*/
+This file is part of the Radar Software Toolkit (RST).
 
-/* Notes:
+RST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/license
+
+Notes:
  *
  * - add sort by velocity so largest velocities plotted last
  * - added old_aacgm parameter
--- codebase/superdarn/src.lib/tk/shf.1.10/src/calc_bmag.c
+++ codebase/superdarn/src.lib/tk/shf.1.10/src/calc_bmag.c
@@ -1,11 +1,25 @@
 /* calc_bmag.c
    ===========
    Author: E.D.P.Cousins and others
+
+This file is part of the Radar Software Toolkit (RST).
+
+RST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/license
+
+
 */
 
-/*
-   See license.txt
-*/
 
 #include <stdlib.h>
 #include <stdio.h>
