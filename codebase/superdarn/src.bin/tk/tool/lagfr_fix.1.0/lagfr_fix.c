427ddacebb0c700bcebff0fa08dee1b4aad332e4
mts299 <marina.t.schmidt@gmail.com>
2021-03-03 11:08:28 -0600

    deleted reference RST license line and added the disclaimer

--- codebase/superdarn/src.bin/tk/tool/lagfr_fix.1.0/lagfr_fix.c
+++ codebase/superdarn/src.bin/tk/tool/lagfr_fix.1.0/lagfr_fix.c
@@ -4,9 +4,22 @@
 */
 
 /*
- (c) 2010 JHU/APL & Others - Please Consult LICENSE.superdarn-rst.3.2-beta-4-g32f7302.txt for more information.
+ (c) 2010 JHU/APL & Others 
 
+This file is part of the Radar Software Toolkit (RST).
 
+RST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 */
 
