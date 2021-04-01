720235b6eb893c5633b6c31adf10eed3c1e56449
mts299 <marina.t.schmidt@gmail.com>
2021-04-01 14:16:13 -0600

    removed duplicates

--- codebase/superdarn/src.lib/tk/idl/fitidl.1.5/src/fitidl.c
+++ codebase/superdarn/src.lib/tk/idl/fitidl.1.5/src/fitidl.c
@@ -2,7 +2,6 @@
    ======== 
    Author R.J.Barnes
 */
-
 /*
  Copyright (C) <year>  <name of author>
 
@@ -23,21 +22,6 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
-
-Modifications:
-
-RST is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 Modifications:
