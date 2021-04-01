64e386ea3349fc5fb3469e2a7039229b0a9d1752
mts299 <marina.t.schmidt@gmail.com>
2021-04-01 12:48:46 -0600

    added disclaimers on GPL and altered libraries to LGPL

--- codebase/general/src.lib/grplotlib.1.4/src/grplot_key.c
+++ codebase/general/src.lib/grplotlib.1.4/src/grplot_key.c
@@ -1,30 +1,25 @@
 /* grplot_key.c
    ============
    Author: R.J.Barnes
-*/
+Copyright (C) <year>  <name of author>
 
-/*
- LICENSE AND DISCLAIMER
- 
- Copyright (c) 2012 The Johns Hopkins University/Applied Physics Laboratory
- 
- This file is part of the Radar Software Toolkit (RST).
- 
- RST is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- any later version.
- 
- RST is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with RST.  If not, see <http://www.gnu.org/licenses/>.
- 
- 
- 
+This file is part of the Radar Software Toolkit (RST).
+
+RST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+Modifications:
 */
 
 
