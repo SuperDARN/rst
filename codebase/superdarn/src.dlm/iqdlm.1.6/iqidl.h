e93105cce7e5fad73d76ad0d1b339865f75e80b9
mts299 <marina.t.schmidt@gmail.com>
2021-04-05 13:56:25 -0600

    fixed commenting error in header files for dlms

--- codebase/superdarn/src.dlm/iqdlm.1.6/iqidl.h
+++ codebase/superdarn/src.dlm/iqdlm.1.6/iqidl.h
@@ -23,7 +23,7 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 Modifications:
- 
+*/
 
 #ifndef _IQIDL_H
 #define _IQIDL_H
