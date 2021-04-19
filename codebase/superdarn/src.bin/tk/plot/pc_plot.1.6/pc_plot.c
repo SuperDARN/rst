d8b5146a3bd201c71f28c50e66635eaba2374c69
egthomas <evan.g.thomas@dartmouth.edu>
2021-04-19 09:52:40 -0400

    Fixing pointer freeing error in hmb_plot and pc_plot

--- codebase/superdarn/src.bin/tk/plot/pc_plot.1.6/pc_plot.c
+++ codebase/superdarn/src.bin/tk/plot/pc_plot.1.6/pc_plot.c
@@ -798,10 +798,12 @@ int main(int argc,char *argv[]) {
     else FrameBufferSavePNG(img,stdout);
   #endif
 
-  free(fname);
+  if ((cfname !=NULL) && (arg<optf->argc)) {
+    free(fname);
+  }
   return 0;
 
 
- }
+}
 
 
