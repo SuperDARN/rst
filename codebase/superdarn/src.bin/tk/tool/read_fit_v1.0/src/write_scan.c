e273c6c753ed12ba17f3c60eb7e3daae06e729a6
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-04-28 14:18:07 -0400

    BUG: debugged read_fit
    Improved read_fit output.

--- codebase/superdarn/src.bin/tk/tool/read_fit_v1.0/src/write_scan.c
+++ codebase/superdarn/src.bin/tk/tool/read_fit_v1.0/src/write_scan.c
@@ -92,14 +92,14 @@ void write_scan(FILE *fp, struct RadarScan *scan, unsigned char vb, char *vbuf)
   char scan_info[10], bm_info[1000], rng_info[1000];
 
   struct RadarBeam *bm;
-  struct RadarCell rng;
+  struct FitRange rng;
 
   /* If there is no scan data, print the header information */
   if(scan == NULL)
     {
       sprintf(scan_info, "#STID");
       sprintf(bm_info, "DATE TIME SCAN BMNUM BMAZM CPID INTT_SC INTT_US NAVE FRANG RSEP RXRISE FREQ NOISE ATTEN CHANNEL NRANG");
-      sprintf(rng_info, "RG GFLG P_0 P_0_ERR V V_ERR W_L W_L_ERR P_L P_L_ERR");
+      sprintf(rng_info, "RG GFLG QFLG P_0 PHI0 PHI0_ERR V V_ERR W_L W_L_ERR P_L P_L_ERR W_S W_S_ERR P_S P_S_ERR");
 
       fprintf(fp, "%s %s %s\n", scan_info, bm_info, rng_info);
     }
@@ -118,9 +118,10 @@ void write_scan(FILE *fp, struct RadarScan *scan, unsigned char vb, char *vbuf)
 	  sprintf(bm_info, "%04d-%02d-%02d %02d:%02d:%02.0f", yr, mo, dy, hr,
 		  mt, sc);
 	  sprintf(bm_info, "%s %d %d %0.3f %d %d %d %d %d %d %d %d %d %d %d %d",
-		  bm_info, bm->scan, bm->bm, bm->bmazm, bm->cpid, bm->intt.sc,
-		  bm->intt.us, bm->nave, bm->frang, bm->rsep, bm->rxrise,
-		  bm->freq, bm->noise, bm->atten, bm->channel, bm->nrang);
+		  bm_info, bm->prm.scan, bm->prm.bmnum, bm->prm.bmazm,
+		  bm->prm.cp, bm->prm.intt.sc, bm->prm.intt.us, bm->prm.nave,
+		  bm->prm.frang, bm->prm.rsep, bm->prm.rxrise, bm->prm.tfreq,
+		  bm->prm.noise, bm->prm.atten, bm->prm.channel, bm->prm.nrang);
 
 	  /* Cycle through all the range gates */
 	  for(rg=0; rg<bm->nrang; rg++)
@@ -129,9 +130,11 @@ void write_scan(FILE *fp, struct RadarScan *scan, unsigned char vb, char *vbuf)
 	      if(bm->sct[rg] == 1)
 		{
 		  rng = bm->rng[rg];
-		  sprintf(rng_info, "%d %d %f %f %f %f %f %f %f %f", rg,
-			  rng.gsct, rng.p_0, rng.p_0_e, rng.v, rng.v_e,
-			  rng.w_l, rng.w_l_e, rng.p_l, rng.p_l_e);
+		  sprintf(rng_info,
+			  "%d %d %d %f %f %f %f %f %f %f %f %f %f %f %f %f", rg,
+			  rng.gsct, rng.qflg, rng.p_0, rng.phi0, rng.phi0_e,
+			  rng.v, rng.v_e, rng.w_l, rng.w_l_e, rng.p_l,
+			  rng.p_l_e, rng.w_s, rng.w_s_e, rng.p_s, rng.p_s_e);
 	      
 		  fprintf(fp, "%s %s %s\n", scan_info, bm_info, rng_info);
 		}
