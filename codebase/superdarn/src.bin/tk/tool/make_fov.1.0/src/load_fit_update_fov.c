bccd6329357ca62d491bbae9d0d8819b8643d873
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-07-21 08:15:52 -0400

    ENH: removed cfit support
    CFit files don't have elevation angles, so don't need to be supported in this routine.

--- codebase/superdarn/src.bin/tk/tool/make_fov.1.0/src/load_fit_update_fov.c
+++ codebase/superdarn/src.bin/tk/tool/make_fov.1.0/src/load_fit_update_fov.c
@@ -41,11 +41,6 @@
 #include "fitseek.h"
 #include "oldfitread.h"
 #include "oldfitscan.h"
-#include "cfitdata.h"
-#include "cfitindex.h"
-#include "cfitread.h"
-#include "cfitscan.h"
-#include "cfitseek.h"
 #include "multbsid.h"
 
 /**
@@ -61,7 +56,6 @@
  *             etime       - Ending time
  *             edate       - Ending date
  *             extime      - Time extent (zero to use start/end date-times)
- *             fitflg      - 1 if a fit file, 0 if a cfit file
  *             nsflg       - If 1, exclude data where scan flag is -1
  *             vb          - Verbosity flag
  *             vbuf        - Verbosity buffer
@@ -95,15 +89,15 @@
 
 int load_fit_update_fov(int fnum, int channel, int channel_fix, int old,
 			int tlen, double stime, double sdate, double etime,
-			double edate, double extime, unsigned char fitflg,
-			unsigned char nsflg, unsigned char vb, char *vbuf,
-			char *iname, char **dnames, short int tdiff_flag,
-			double tdiff, short int strict_gs, int freq_min,
-			int freq_max, int min_pnts, int D_nrg, int E_nrg,
-			int F_nrg, int far_nrg, int D_rgmax, int E_rgmax,
-			int F_rgmax, float D_hmin, float D_hmax, float E_hmax,
-			float F_hmax, float D_vh_box, float E_vh_box,
-			float F_vh_box, float far_vh_box, float max_hop,
+			double edate, double extime, unsigned char nsflg,
+			unsigned char vb, char *vbuf, char *iname,
+			char **dnames, short int tdiff_flag, double tdiff,
+			short int strict_gs, int freq_min, int freq_max,
+			int min_pnts, int D_nrg, int E_nrg, int F_nrg,
+			int far_nrg, int D_rgmax, int E_rgmax, int F_rgmax,
+			float D_hmin, float D_hmax, float E_hmax, float F_hmax,
+			float D_vh_box, float E_vh_box, float F_vh_box,
+			float far_vh_box, float max_hop,
 			struct MultFitBSID *mult_bsid)
 {
   int yr, mo, dy, hr, mt, inum, ret_flg=0, state=0, syncflg=1;
@@ -115,10 +109,8 @@ int load_fit_update_fov(int fnum, int channel, int channel_fix, int old,
   struct RadarSite *site;
   struct RadarParm *prm;
   struct FitData *fit;
-  struct CFitdata *cfit;
   struct FitIndex *inx;
   struct OldFitFp *oldfitfp=NULL;
-  struct CFitfp *cfitfp=NULL;
   struct RadarScan *scan;
 
   int exclude_outofscan(struct RadarScan *ptr);
@@ -132,13 +124,12 @@ int load_fit_update_fov(int fnum, int channel, int channel_fix, int old,
 		       float far_vh_box, float max_hop, struct RadarScan *scan,
 		       struct RadarSite *hard, struct MultFitBSID *mult_bsid);
 
-  /* Initialize radar parameter and fit/cfit structures */
+  /* Initialize radar parameter and fit structures */
   site = (struct RadarSite *)malloc(sizeof(struct RadarSite));
   memset(site, 0, sizeof(struct RadarSite));
   scan = RadarScanMake();
   prm = RadarParmMake();
   fit = FitMake();
-  cfit = CFitMake();
 
   /* Cycle through all of the fit files */
   for(inum=0; inum < fnum; inum++)
@@ -147,88 +138,64 @@ int load_fit_update_fov(int fnum, int channel, int channel_fix, int old,
       /* Assign the input and index file names */
       fprintf(stderr, "Opening file:%s\n", dnames[inum]);
 
-      /* Open the fit, fitacf, or cfit file and read the first scan */
-      if(fitflg)
+      /* Open the fit or fitacf file, reading the first scan */
+      if(old)
 	{
-	  /* Input file is in fit or fitacf format */
-	  if(old)
-	    {
-	      /* Input file is in fit format. Open the fit file for reading */
-	      if((oldfitfp = OldFitOpen(dnames[inum], iname)) == NULL)
-		{
-		  fprintf(stderr, "File not found.\n");
-		  exit(-1);
-                }
-
-	      /* Read first available radar scan in fit file (will use scan
-		 flag if tlen not provided) */
-	      ret_flg = OldFitReadRadarScan(oldfitfp, &state, scan, prm, fit,
-					    tlen, syncflg, channel);
-
-	      /* Verify that scan was properly read */
-	      if(ret_flg == -1)
-		{
-		  fprintf(stderr,"Error reading file.\n");
-		  exit(-1);
-                }
-            }
-	  else
-	    {
-	      /* Input file is in fitacf format */
-
-	      /* Check if index file provided */
-	      if(iname != NULL)
-		{
-		  /* Open the index file for reading */
-		  if((fp = fopen(iname, "r")) == NULL)
-		    fprintf(stderr, "Could not open index.\n");
-
-		  else
-		    {
-		      if((inx = FitIndexFload(fp)) == NULL)
-			fprintf(stderr, "Error loading index.\n");
-		      fclose(fp);
-                    }
-                }
-
-	      /* Open the fitacf file for reading */
-	      if((fitfp = fopen(dnames[inum], "r")) == NULL)
-		{
-		  fprintf(stderr, "File not found.\n");
-		  exit(-1);
-                }
-
-	      /* Read first available radar scan in fitacf file (will use scan
-		 flag if tlen not provided) */
-	      ret_flg = FitFreadRadarScan(fitfp, &state, scan, prm, fit,
-					  tlen, syncflg, channel);
-
-	      /* Verify that scan was properly read */
-	      if(ret_flg == -1)
-		{
-		  fprintf(stderr, "Error reading file.\n");
-		  exit(-1);
-                }
-            }
-        }
-      else
-	{
-	  /* Input file is in cfit or format. Open the cfit file for reading */
-	  if((cfitfp=CFitOpen(dnames[inum])) == NULL)
+	  /* Input file is in fit format. Open the fit file for reading */
+	  if((oldfitfp = OldFitOpen(dnames[inum], iname)) == NULL)
 	    {
 	      fprintf(stderr, "File not found.\n");
 	      exit(-1);
-            }
+	    }
 
-	  /* Verify that the cfit file was properly opened */
-	  ret_flg = CFitReadRadarScan(cfitfp, &state, scan, cfit, tlen, syncflg,
-				      channel);
+	  /* Read first available radar scan in fit file (will use scan
+	     flag if tlen not provided) */
+	  ret_flg = OldFitReadRadarScan(oldfitfp, &state, scan, prm, fit,
+					tlen, syncflg, channel);
+
+	  /* Verify that scan was properly read */
+	  if(ret_flg == -1)
+	    {
+	      fprintf(stderr,"Error reading file.\n");
+	      exit(-1);
+	    }
+	}
+      else
+	{
+	  /* Input file is in fitacf format. Check if index file provided */
+	  if(iname != NULL)
+	    {
+	      /* Open the index file for reading */
+	      if((fp = fopen(iname, "r")) == NULL)
+		fprintf(stderr, "Could not open index.\n");
+
+	      else
+		{
+		  if((inx = FitIndexFload(fp)) == NULL)
+		    fprintf(stderr, "Error loading index.\n");
+		  fclose(fp);
+		}
+	    }
+
+	  /* Open the fitacf file for reading */
+	  if((fitfp = fopen(dnames[inum], "r")) == NULL)
+	    {
+	      fprintf(stderr, "File not found.\n");
+	      exit(-1);
+	    }
+
+	  /* Read first available radar scan in fitacf file (will use scan
+	     flag if tlen not provided) */
+	  ret_flg = FitFreadRadarScan(fitfp, &state, scan, prm, fit,
+				      tlen, syncflg, channel);
+
+	  /* Verify that scan was properly read */
 	  if(ret_flg == -1)
 	    {
 	      fprintf(stderr, "Error reading file.\n");
 	      exit(-1);
-            }
-        }
+	    }
+	}
 
       /* If either start time or date not provided as input then determine it */
       if((stime != -1) || (sdate != -1))
@@ -249,82 +216,47 @@ int load_fit_update_fov(int fnum, int channel, int channel_fix, int old,
 
 	  /* Search for index of corresponding record in input file given */
 	  /* start time                                                   */
-	  if(fitflg)
+	  if(old)
+	    ret_flg = OldFitSeek(oldfitfp, yr, mo, dy, hr, mt, sc, NULL);
+	  else ret_flg = FitFseek(fitfp, yr, mo, dy, hr, mt, sc, NULL, inx);
+
+	  /* If a matching record could not be found then exit */
+	  if(ret_flg == -1)
 	    {
-	      /* Input file is in fit or fitacf format */
+	      fprintf(stderr, "File doesn't contain the requested interval.\n");
+	      exit(-1);
+	    }
+
+	  /* If using scan flag instead of tlen then continue to read
+	     fit records until reaching the beginning of a new scan */
+	  if(tlen==0)
+	    {
 	      if(old)
-		ret_flg = OldFitSeek(oldfitfp, yr, mo, dy, hr, mt, sc, NULL);
-	      else ret_flg = FitFseek(fitfp, yr, mo, dy, hr, mt, sc, NULL, inx);
-
-	      /* If a matching record could not be found then exit */
-	      if(ret_flg == -1)
 		{
-		  fprintf(stderr,
-			  "File does not contain the requested interval.\n");
-		  exit(-1);
-                }
-
-	      /* If using scan flag instead of tlen then continue to read
-		 fit records until reaching the beginning of a new scan */
-	      if(tlen==0)
-		{
-		  if(old)
+		  while((ret_flg = OldFitRead(oldfitfp, prm, fit)) != -1)
 		    {
-		      while((ret_flg = OldFitRead(oldfitfp,prm,fit)) != -1)
-			{
-			  if(abs(prm->scan) == 1) break;
-                        }
-                    }
-		  else
-		    {
-		      while((ret_flg = FitFread(fitfp, prm, fit)) != -1)
-			{
-			  if(abs(prm->scan) == 1) break;
-                        }
-                    }
-                }
-	      else state=0;
-
-	      /* Read the first full scan of data from open fit or fitacf file
-		 corresponding to the requested start date and time */
-	      if(old)
-		ret_flg = OldFitReadRadarScan(oldfitfp, &state, scan, prm, fit,
-					      tlen, syncflg, channel);
+		      if(abs(prm->scan) == 1) break;
+		    }
+		}
 	      else
-		ret_flg = FitFreadRadarScan(fitfp, &state, scan, prm, fit, tlen,
-					    syncflg, channel);
-            }
-	  else
-	    {
-	      /* Input file is in cfit format */
-	      ret_flg = CFitSeek(cfitfp, yr, mo, dy, hr, mt, sc, NULL, NULL);
-
-	      /* If a matching record could not be found then exit */
-	      if(ret_flg == -1)
 		{
-		  fprintf(stderr,
-			  "File does not contain the requested interval.\n");
-		  exit(-1);
-                }
-
-	      /* If using scan flag instead of tlen then continue to read
-		 cfit records until reaching the beginning of a new scan */
-	      if(tlen==0)
-		{
-		  while((ret_flg = CFitRead(cfitfp, cfit)) != -1)
+		  while((ret_flg = FitFread(fitfp, prm, fit)) != -1)
 		    {
-		      if(cfit->scan == 1) break;
-                    }
-                }
-	      else state = 0;
+		      if(abs(prm->scan) == 1) break;
+		    }
+		}
+	    }
+	  else state=0;
 
-	      /* Read the first full scan of data from open cfit file
-		 corresponding to the start date and time */
-	      ret_flg = CFitReadRadarScan(cfitfp, &state, scan, cfit, tlen,
-					  syncflg, channel);
-
-            }
-        }
+	  /* Read the first full scan of data from open fit or fitacf file
+	     corresponding to the requested start date and time */
+	  if(old)
+	    ret_flg = OldFitReadRadarScan(oldfitfp, &state, scan, prm, fit,
+					  tlen, syncflg, channel);
+	  else
+	    ret_flg = FitFreadRadarScan(fitfp, &state, scan, prm, fit, tlen,
+					syncflg, channel);
+	}
       else stime = scan->st_time;   /* If the start date and time are not
 				       provided, use the time of the input
 				       file's first record */
@@ -359,25 +291,20 @@ int load_fit_update_fov(int fnum, int channel, int channel_fix, int old,
 	    }
 
 	  /* Update the backscatter FoV data using only data from this scan */
+	  printf("TEST: UPDATE SCAN!\n");fflush(stdout);
 	  UpdateScanBSFoV(strict_gs, freq_min, freq_max, min_pnts, D_nrg, E_nrg,
 			  F_nrg, far_nrg, D_rgmax, E_rgmax, F_rgmax, D_hmin,
 			  D_hmax, E_hmax, F_hmax, D_vh_box, E_vh_box, F_vh_box,
 			  far_vh_box, max_hop, scan, site, mult_bsid);
-
+	  printf("TEST: SCAN UPDATED!\n");fflush(stdout);
 
 	  /* Read next radar scan from input file */
-	  if(fitflg)
-	    {
-	      if(old)
-		ret_flg = OldFitReadRadarScan(oldfitfp, &state, scan, prm, fit,
-					      tlen, syncflg, channel);
-	      else
-		ret_flg = FitFreadRadarScan(fitfp, &state, scan, prm, fit,
-					    tlen, syncflg, channel);
-            }
+	  if(old)
+	    ret_flg = OldFitReadRadarScan(oldfitfp, &state, scan, prm, fit,
+					  tlen, syncflg, channel);
 	  else
-	    ret_flg = CFitReadRadarScan(cfitfp, &state, scan, cfit, tlen,
-					syncflg, channel);
+	    ret_flg = FitFreadRadarScan(fitfp, &state, scan, prm, fit,
+					tlen, syncflg, channel);
 
 	  /* If scan data is beyond end time then break out of loop */
 	  if((etime != -1) && (scan->st_time > etime)) break;
@@ -385,17 +312,15 @@ int load_fit_update_fov(int fnum, int channel, int channel_fix, int old,
         } while (ret_flg != -1);
 
       /* Close the input file */
-      if(fitflg)
-	{
-	  if(old) OldFitClose(oldfitfp);
-	  else fclose(fitfp);
-        }
-      else CFitClose(cfitfp);
+      if(old) OldFitClose(oldfitfp);
+      else fclose(fitfp);
     }
 
+  printf("TEST FREE SITE\n");fflush(stdout);
   /* Free the local pointers */
   free(site);
 
   /* Return with the status flag */
+  printf("TEST RETURN SCAN: %d\n", ret_flg);fflush(stdout);
   return(ret_flg);
 }
