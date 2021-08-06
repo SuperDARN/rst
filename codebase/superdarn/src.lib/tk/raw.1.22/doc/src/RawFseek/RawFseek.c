8c440235df05502cb53a4db9813ed5e2f3e8441b
Emma Bland <emma.bland@unis.no>
2021-08-06 11:08:23 +0200

    Remove duplicate copyright notices - raw-doc

--- codebase/superdarn/src.lib/tk/raw.1.22/doc/src/RawFseek/RawFseek.c
+++ codebase/superdarn/src.lib/tk/raw.1.22/doc/src/RawFseek/RawFseek.c
@@ -1,8 +1,8 @@
 /* RawFseek.c
    ==========
    Author: R.J.Barnes
- Copyright (c) 2012 The Johns Hopkins University/Applied Physics Laboratory
- Copyright (c) 2012 The Johns Hopkins University/Applied Physics Laboratory
+
+Copyright (c) 2012 The Johns Hopkins University/Applied Physics Laboratory
 
 This file is part of the Radar Software Toolkit (RST).
 
