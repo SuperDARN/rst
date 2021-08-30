3ebdc4e5cfc3ba27467d2a59cd3429b722a6d155
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-08-30 10:02:22 -0400

    ENH: added channel library
    Added a library for processing command-line channel inputs.

---
+++ codebase/superdarn/src.lib/tk/channel.1.0/include/channel.h
@@ -0,0 +1,31 @@
+/* channel.h
+   ========
+   Author: A.G. Burrell
+
+This file is part of the Radar Software Toolkit (RST).
+
+RST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+Modifications:
+
+*/
+
+
+#ifndef _CHANNEL_H
+#define _CHANNEL_H
+
+int set_stereo_channel(char *chnstr);
+int set_fix_channel(char *chnstr_fix);
+
+#endif
