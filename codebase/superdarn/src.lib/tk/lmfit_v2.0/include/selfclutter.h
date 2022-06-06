9ca9c4ead5c0007c4d897d6efeb20d3d03803656
Emma Bland <emma.bland@unis.no>
2022-06-06 17:03:31 +0200

    Add license notices

--- codebase/superdarn/src.lib/tk/lmfit_v2.0/include/selfclutter.h
+++ codebase/superdarn/src.lib/tk/lmfit_v2.0/include/selfclutter.h
@@ -1,30 +1,28 @@
 /* selfclutter.h
    =====
-   Author: A.S. Reimer
-*/
 
-/* 
- LICENSE AND DISCLAIMER
- 
- Copyright (c) 2014 The Institute for Space and Atmospheric Study at 
- the University of Saskatchewan
- 
- This file is part of the Radar Software Toolkit (RST).
- 
- RST is free software: you can redistribute it and/or modify
- it under the terms of the GNU Lesser General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- any later version.
- 
- RST is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Lesser General Public License for more details.
- 
- You should have received a copy of the GNU Lesser General Public License
- along with RST.  If not, see <http://www.gnu.org/licenses/>.
- 
- 
+//TODO Add copyright notice
+
+   Author: A.S. Reimer
+
+
+This file is part of the Radar Software Toolkit (RST).
+
+RST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+Modifications:
+
  
 */
 
