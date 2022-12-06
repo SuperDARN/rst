0e683a9f2c78ac73c38b435e2da229dc0e9839cf
egthomas <evan.g.thomas@dartmouth.edu>
2022-12-06 10:33:22 -0500

    Modifying field_plot to add new -minbeam and -maxbeam options for plotting radar data using fewer than the maximum number of beams listed in the radar hardware file

--- codebase/superdarn/src.bin/tk/plot/field_plot.1.12/plot_outline.h
+++ codebase/superdarn/src.bin/tk/plot/field_plot.1.12/plot_outline.h
@@ -29,14 +29,15 @@ Modifications:
 
 
 void plot_outline(struct Plot *plot,struct RadarBeam *sbm,
-		  struct GeoLocBeam *gbm,float latmin,int magflg,int maxbeam,
-               float xoff,float yoff,float wdt,float hgt,
-               int (*trnf)(int,void *,int,void *,void *data),void *data,
-	       unsigned int color);
+                  struct GeoLocBeam *gbm,float latmin,int magflg,
+                  int minbeam, int maxbeam,
+                  float xoff,float yoff,float wdt,float hgt,
+                  int (*trnf)(int,void *,int,void *,void *data),
+                  void *data,unsigned int color);
 
 void plot_filled(struct Plot *plot,struct RadarBeam *sbm,
-               struct GeoLocBeam *gbm,float latmin,int magflg,
-               float xoff,float yoff,float wdt,float hgt,
-               int (*trnf)(int,void *,int,void *,void *data),void *data,
-	       unsigned int color);
+                 struct GeoLocBeam *gbm,float latmin,int magflg,
+                 float xoff,float yoff,float wdt,float hgt,
+                 int (*trnf)(int,void *,int,void *,void *data),void *data,
+                 unsigned int color);
 
