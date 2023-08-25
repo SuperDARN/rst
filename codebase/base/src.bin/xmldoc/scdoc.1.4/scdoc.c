e9b0a4eedbbbf7855d50b10a81af11b8bc4347fc
egthomas <evan.g.thomas@dartmouth.edu>
2023-08-25 11:03:39 -0400

    Modifying scdoc and xmldoc binaries to include option error handling support

--- codebase/base/src.bin/xmldoc/scdoc.1.4/scdoc.c
+++ codebase/base/src.bin/xmldoc/scdoc.1.4/scdoc.c
@@ -43,6 +43,11 @@
 int arg;
 struct OptionData opt;
 
+int rst_opterr(char *txt) {
+  fprintf(stderr,"Option not recognized: %s\n",txt);
+  fprintf(stderr,"Please try: scdoc --help\n");
+  return(-1);
+}
 
 struct {
   int num;
@@ -181,6 +186,7 @@ int main(int argc,char *argv[]) {
 
   unsigned char help=0;
   unsigned char option=0;
+  unsigned char version=0;
 
   struct OptionText *ignore=NULL;
   struct OptionText *remove=NULL;
@@ -192,10 +198,15 @@ int main(int argc,char *argv[]) {
  
   OptionAdd(&opt,"-help",'x',&help);
   OptionAdd(&opt,"-option",'x',&option);
+  OptionAdd(&opt,"-version",'x',&version);
   OptionAdd(&opt,"i",'a',&ignore);
   OptionAdd(&opt,"r",'a',&remove);
 
-  arg=OptionProcess(1,argc,argv,&opt,NULL);   
+  arg=OptionProcess(1,argc,argv,&opt,rst_opterr);
+
+  if (arg==-1) {
+    exit(-1);
+  }
 
   if (help==1) {
     OptionPrintInfo(stdout,hlpstr);
@@ -204,6 +215,11 @@ int main(int argc,char *argv[]) {
 
   if (option==1) {
     OptionDump(stdout,&opt);
+    exit(0);
+  }
+
+  if (version==1) {
+    OptionVersion(stdout);
     exit(0);
   }
 
