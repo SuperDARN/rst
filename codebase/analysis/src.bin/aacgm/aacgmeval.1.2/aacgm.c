4820dbed139ce50a7ab72c025738b1f2884bc4c8
egthomas <evan.g.thomas@dartmouth.edu>
2024-01-22 12:28:23 -0500

    Fixing out of bounds array access when reading text file in a few binaries

--- codebase/analysis/src.bin/aacgm/aacgmeval.1.2/aacgm.c
+++ codebase/analysis/src.bin/aacgm/aacgmeval.1.2/aacgm.c
@@ -186,8 +186,8 @@ int main(int argc,char *argv[]) {
     if (fp==NULL) exit(1);
     while(fgets(txt,255,fp) !=NULL) {
       for (c=0;(txt[c] !=0) && (c<256);c++) 
-      if (txt[c]=='#') break;
-      if (txt[c]=='#') continue;
+        if (txt[c]=='#') break;
+      if ((c<256) && (txt[c]=='#')) continue;
       if (sscanf(txt,"%lf %lf %lf\n",
           &ilat,&ilon,&alt) !=3) continue;
       if (old_aacgm) AACGMConvert(ilat,ilon,alt,&olat,&olon,&r,flag);
