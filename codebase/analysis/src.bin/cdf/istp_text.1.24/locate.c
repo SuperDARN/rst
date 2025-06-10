b484790ba35b1be046a030a76b04eb28c549f2ee
Oleg I.Berngardt <47649740+berng@users.noreply.github.com>
2025-06-10 11:54:59 +0800

    Update locate.c
    To compile under Ubuntu 24.

--- codebase/analysis/src.bin/cdf/istp_text.1.24/locate.c
+++ codebase/analysis/src.bin/cdf/istp_text.1.24/locate.c
@@ -180,7 +180,7 @@ struct file_list *locate_files(char *fpath,char *prefix,
   
   for (yr=syr;yr<=eyr;yr++) {
     
-    sprintf(dir,"%s/%.4d",fpath,(double) yr);
+    sprintf(dir,"%s/%.4d",fpath, yr);
    
     status=scandir(dir,&dlist,dsel,alphasort);
     
